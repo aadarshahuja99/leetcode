8e8746e778c0f6efcc8c3686996d5f02703f5d60
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-01-22 00:53:38 +0530

    Time: 597 ms (18.17%), Space: 76.7 MB (26.63%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        // Create a MST using Kruskal's algorithm. n*n TC to create edges. Sort all the edges. Then, use a disjoint set to connect edges. total TC: (n*n)log(n*n). Total SC: n*n to store all the edges. Since, this is a dense graph. Use Prim's algo for better performance
+        if(points.length == 1)
+        {
+            return 0;
+        }
+        int n = points.length;
+        int size = (n)*(n-1)/2;
+        PriorityQueue<QueueElement> pq = new PriorityQueue<QueueElement>(new Comparator<QueueElement>() {
+            public int compare(QueueElement q1, QueueElement q2)
+            {
+                return q1.getDistance()-q2.getDistance();
+            }
+        });
+        int k=0;
+        ArrayList<ArrayList<QueueElement>> adjList = new ArrayList<ArrayList<QueueElement>>();
+        for(int i=0; i<n; i++){
+            adjList.add(new ArrayList<QueueElement>());
+        }
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int d = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+                adjList.get(i).add(new QueueElement(j,d));
+                adjList.get(j).add(new QueueElement(i,d));
+            }
+        }
+        int[] visited = new int[n];
+        pq.add(new QueueElement(0,0));
+        int ans = 0;
+        while(pq.size() > 0)
+        {
+            var top = pq.poll();
+            int dist = top.getDistance();
+            int current = top.getNode();
+            // System.out.println("adding "+dist+" from current: "+current);
+            if(visited[current] == 1)
+            {
+                continue;
+            }
+            ans += dist;
+            visited[current] = 1;
+            for(QueueElement node : adjList.get(current))
+            {
+                if(visited[node.getNode()] == 0)
+                {
+                    pq.add(node);
+                }
+            }
+        }
+        return ans;
+    }
+    class QueueElement
+    {
+        private int node;
+        private int dist;
+        public QueueElement(int n, int d)
+        {
+            node=n;
+            dist=d;
+        }
+        public int getDistance()
+        {
+            return dist;
+        }
+        public int getNode()
+        {
+            return node;
+        }
+    }
+}
