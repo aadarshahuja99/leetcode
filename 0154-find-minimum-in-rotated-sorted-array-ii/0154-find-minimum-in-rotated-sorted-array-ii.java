9991b1bfddfcc3ad776ecd254dfe2ccbba98e405
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-01-28 16:09:08 +0530

    Time: 0 ms (100%), Space: 43.6 MB (11.01%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        int min = Integer.MAX_VALUE;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(nums[start] == nums[mid] && nums[mid] == nums[end])
+            {
+                int current = nums[mid];
+                if(current < min)
+                {
+                    min = current;
+                }
+                start++;
+                end--;
+                continue;
+            }
+            if(nums[mid] == nums[end])
+            {
+                if(nums[start] > nums[mid])
+                {
+                    int current = nums[mid];
+                    if(current < min)
+                    {
+                        min = current;
+                    }
+                    end = mid-1;
+                }
+                else
+                {
+                    int current = nums[start];
+                    if(current < min)
+                    {
+                        min = current;
+                    }
+                    start = mid+1;
+                }
+            }
+            else
+            {
+                if(nums[end] > nums[mid])
+                {
+                    int current = nums[mid];
+                    if(current < min)
+                    {
+                        min = current;
+                    }
+                    end = mid-1;
+                }
+                else
+                {
+                    int current = nums[start];
+                    if(current < min)
+                    {
+                        min = current;
+                    }
+                    start = mid+1;
+                }
+            }
+        }
+        return min;
+    }
+}
