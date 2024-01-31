012c8dcc723461c3033cf56438e4712e5b4ce79e
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-01-31 06:48:08 +0530

    Time: 8 ms (54.68%), Space: 40.2 MB (68.63%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,36 +1,27 @@
 class Solution {
     int max = 1001;
     public int minSteps(int n) {
-        int[][] dp = new int[n+1][n+1];
-        for(int[] row : dp)
-        {
-            Arrays.fill(row,-1);
-        }
-        return getAns(1,0,n,dp);
+        return getAns(1,0,n);
     }
-    private int getAns(int current, int lastLength, int n, int[][] dp)
+    private int getAns(int current, int lastLength, int n)
     {
         if(current == n)
         {
             return 0;
         }
-        if(dp[current][lastLength] != -1)
-        {
-            return dp[current][lastLength];
-        }
         if(lastLength == 0)
         {
-            return (dp[current][lastLength] = 1 + getAns(current,1,n,dp));
+            return 1 + getAns(current,1,n);
         }
         if(lastLength + current <= n)
         {
             if(lastLength != current)
             {
-                return (dp[current][lastLength] = 1 + Math.min(getAns(current+lastLength,lastLength,n,dp), getAns(current,current,n,dp)));
+                return 1 + Math.min(getAns(current+lastLength,lastLength,n), getAns(current,current,n));
             }
-            return (dp[current][lastLength] = 1 + getAns(current+lastLength,lastLength,n,dp));
+            return 1 + getAns(current+lastLength,lastLength,n);
         }
         // if no further operations can be done to get to n, return max val indicating that it is not possible to get to n.
-        return (dp[current][lastLength] = max);
+        return max;
     }
 }
