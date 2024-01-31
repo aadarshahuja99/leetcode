14cd972f53a647cc38c2e4feb5d92d88a4c165a9
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-01-31 06:40:55 +0530

    Time: 7 ms (56.26%), Space: 40.3 MB (55.4%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -0,0 +1,32 @@
+class Solution {
+    int max = 1001;
+    public int minSteps(int n) {
+        // int[][] dp = new int[n+1][n+1];
+        // for(int[] row : dp)
+        // {
+        //     Arrays.fill(row,-1);
+        // }
+        return getAns(1,0,n);
+    }
+    private int getAns(int current, int lastLength, int n)
+    {
+        if(current == n)
+        {
+            return 0;
+        }
+        if(lastLength == 0)
+        {
+            return 1 + getAns(current,1,n);
+        }
+        if(lastLength + current <= n)
+        {
+            if(lastLength != current)
+            {
+                return 1 + Math.min(getAns(current+lastLength,lastLength,n), getAns(current,current,n));
+            }
+            return 1 + getAns(current+lastLength,lastLength,n);
+        }
+        // if no further operations can be done to get to n, return max val indicating that it is not possible to get to n.
+        return max;
+    }
+}
