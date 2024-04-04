42cc54f60da182e466da117811aa872f57c10e69
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-04 22:52:24 +0530

    Time: 19 ms (64.84%), Space: 55 MB (98.17%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        int minElement = nums[0];
+        int maxElement = nums[0];
+        int n = nums.length;
+        for(int i=1; i<n; i++)
+        {
+            minElement = Math.min(nums[i], minElement);
+            maxElement = Math.max(nums[i], maxElement);
+        }
+        int start = minElement;
+        int end = maxElement;
+        int ans = -1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(check(mid,nums,k))
+            {
+                ans = mid;
+                end = mid-1;
+            }
+            else
+            {
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    private boolean check(int current, int[] nums, int k)
+    {
+        int previous = -2;
+        for(int i=0; i<nums.length;)
+        {
+            if(current < nums[i] || i == previous+1)
+            {
+                i+=1;
+                continue;
+            }
+            k--;
+            if(k == 0)
+            {
+                return true;
+            }
+            previous=i;
+            i+=2;
+        }
+        return false;
+    }
+}
