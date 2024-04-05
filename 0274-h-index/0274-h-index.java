7af8da384d3c4d7cce0704d2c92ec16ce1e5de5b
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-05 05:54:22 +0530

    Time: 0 ms (100%), Space: 41.1 MB (86.65%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        // use running sum + array (meetings 2)
+        int max = citations[0];
+        // HashSet<Integer> set = new HashSet<>();
+        // set.add(citations[0]);
+        for(int i=1; i<citations.length; i++)
+        {
+            max = Math.max(max, citations[i]);
+            // set.add(citations[i]);
+        }
+        int[] nums = new int[max+1];
+        for(int citation : citations)
+        {
+            nums[citation]++;
+        }
+        if(nums[nums.length-1] >= nums.length-1)
+        {
+            return nums.length-1;
+        }
+        // for(int num : set)
+        // {
+        //     System.out.println(num);
+        // }
+        for(int i=max-1; i>=0; i--)
+        {
+            nums[i] += nums[i+1];
+            // System.out.println(i+" "+nums[i]);
+            if(nums[i] >= i)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
