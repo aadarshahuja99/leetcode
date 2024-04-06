3253604f362f893260bda9d1679493d2bb809800
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-06 16:13:36 +0530

    Time: 7 ms (12.96%), Space: 41.2 MB (87.65%) - LeetHub

---
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean splitString(String s) {
+        return checkIfTheStringCanBeSplit(0,-1l,0,s);
+    }
+    private boolean checkIfTheStringCanBeSplit(int currentIndex, long lastValue, int numSplits, String s)
+    {
+        if(currentIndex == s.length() && numSplits > 0)
+        {
+            return true;
+        }
+        long currentValue = 0;
+        boolean currentStatus = false;
+        for(int i=currentIndex; i<s.length(); i++)
+        {
+            int iValue = s.charAt(i) - 48;
+            currentValue = currentValue*10 + 1l*iValue;
+            if(((lastValue == -1) || (currentValue == lastValue-1)))
+            {
+                currentStatus = currentStatus || checkIfTheStringCanBeSplit(i+1, currentValue, i == s.length()-1 ? numSplits : numSplits+1, s);
+                if(currentStatus)
+                {
+                    System.out.println("returning true for currentValue " + currentValue);
+                    return true;
+                }
+            }
+        }
+        return currentStatus;
+    }
+}
