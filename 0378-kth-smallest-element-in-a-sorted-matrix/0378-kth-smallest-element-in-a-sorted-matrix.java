d65d1778cb894f697b55dcf937078e5f75acdd32
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-06 11:16:08 +0530

    Time: 19 ms (33.23%), Space: 47.8 MB (86.27%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,21 +1,47 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        if(matrix.length == 1 || k == 1)
-        {
-            return matrix[0][0];
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int[] row : matrix)
-        {
-            for(int element : row)
+        // brute-force. Worst case: (n2)log(n2)
+            // if(matrix.length == 1 || k == 1)
+            // {
+            //     return matrix[0][0];
+            // }
+            // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            // for(int[] row : matrix)
+            // {
+            //     for(int element : row)
+            //     {
+            //         pq.add(element);
+            //         if(pq.size() > k)
+            //         {
+            //             pq.poll();
+            //         }
+            //     }
+            // }
+            // return pq.peek();
+        // more efficient solution (inspired from k sorted arrays problem):
+        // if matrix[i][j] is taken as the next smallest element, add matrix[i][j+1] to the pq. The complexity for this approach is: O(klogN)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] element1, int[] element2)
             {
-                pq.add(element);
-                if(pq.size() > k)
-                {
-                    pq.poll();
-                }
+                return element1[0] - element2[0];
             }
+        });
+        int n = matrix.length;
+        for(int i=0; i<n; i++)
+        {
+            pq.add(new int[] { matrix[i][0], i, 0 });
         }
-        return pq.peek();
+        while(k > 1)
+        {
+            int[] top = pq.poll();
+            int row = top[1];
+            int column = top[2];
+            if(column < n-1)
+            {
+                pq.add(new int[] { matrix[row][column+1], row, column+1 });
+            }
+            k--;
+        }
+        return pq.peek()[0];
     }
 }
