a79dcc1e7313b23a2673840398b3e1ffdc83078c
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-13 06:52:14 +0530

    Time: 6 ms (50.99%), Space: 44.9 MB (5.78%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        // simple range sum problem
+        Arrays.sort(trips, (a,b) -> {
+            return a[1] - b[1];
+        });
+        int[] numberOfPassengers = new int[1001];
+        for(int i=0; i<trips.length; i++)
+        {
+            numberOfPassengers[trips[i][1]] += trips[i][0];
+            numberOfPassengers[trips[i][2]] -= trips[i][0];
+        }
+        int currentSum = 0;
+        for(int i=0; i<=1000; i++)
+        {
+            currentSum += numberOfPassengers[i];
+            if(currentSum > capacity)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
