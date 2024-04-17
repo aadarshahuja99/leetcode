28834f71fb22cd2b8c3134424b8a902088acc9f1
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-17 11:43:31 +0530

    Time: 1 ms (83.18%), Space: 41.3 MB (48.27%) - LeetHub

---
+++ 0388-longest-absolute-file-path/0388-longest-absolute-file-path.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[] { 0, 3 });
+        int maxLength = 0;
+        for(String component : input.split("\n"))
+        {
+            int tabs = getNumberOfTabs(component);
+            while(stack.size() > 0 && tabs <= stack.peek()[0])
+            {
+                stack.pop();
+            }
+            boolean isAFile = isFile(component);
+            if(stack.size() == 0)
+            {
+                // System.out.println((component.length()) + "  " + tabs + " " + maxLength);
+                if(isAFile)
+                {
+                    maxLength = Math.max(maxLength, component.length());
+                }
+                stack.push(new int[] { tabs, component.length() });
+            }
+            else
+            {
+                if(isAFile)
+                {
+                    maxLength = Math.max(maxLength, stack.peek()[1] + component.length());
+                }
+                // System.out.println((component.length() - tabs + stack.peek()[1]) + "  " + tabs + " " + maxLength);
+                stack.push(new int[] { tabs, component.length() + stack.peek()[1] - tabs });
+            }
+        }
+        return maxLength;
+    }
+    private int getNumberOfTabs(String str)
+    {
+        int i=0;
+        while(i < str.length() && str.charAt(i) == '\t')
+        {
+            i++;
+        }
+        return i;
+    }
+    private boolean isFile(String s)
+    {
+        return s.indexOf(".") != -1;
+    }
+}
