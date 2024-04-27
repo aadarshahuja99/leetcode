58eb8fb7b917cdf0f604c457d0ed74ac024f7649
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-04-27 08:54:07 +0530

    Time: 3 ms (16.7%), Space: 44.5 MB (9.59%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> allCombinations = new ArrayList<String>();
+        getParenthesis(0, 0, "", allCombinations, n);
+        return allCombinations;
+    }
+    private void getParenthesis(int currentIndex, int opened, String currentString, List<String> allCombinations, int n)
+    {
+        if(currentIndex == 2*n)
+        {
+            if(opened == 0)
+            {
+                allCombinations.add(currentString);
+            }
+            return;
+        }
+        if(opened > 0)
+        {
+            getParenthesis(currentIndex+1, opened+1, currentString+"(", allCombinations, n);
+            getParenthesis(currentIndex+1, opened-1, currentString+")", allCombinations, n);
+            return;
+        }
+        getParenthesis(currentIndex+1, opened+1, currentString+"(", allCombinations, n);
+    }
+}
