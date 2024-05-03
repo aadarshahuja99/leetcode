8c625e87938980997ecdeaec619f1a2a465cdb1a
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-03 09:58:16 +0530

    Time: 6 ms (53.12%), Space: 65 MB (52.73%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,54 +1,55 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        int i=0;
-        int j=0;
+        int start = 0;
+        int end = 0;
         int max = 0;
         for(int num : nums)
         {
             max = Math.max(max, num);
         }
+        int n = nums.length;
         int countMax = 0;
         long ans = 0;
-        while(j < nums.length)
+        while(end < n)
         {
             if(countMax < k)
             {
-                if(nums[j] == max)
+                if(nums[end] == max)
                 {
                     countMax++;
                 }
-                j++;
+                end++;
             }
             else if(countMax == k)
             {
-                int candidateLength = j-i;
-                int rightRem = nums.length-candidateLength-i;
-                // System.out.println(i+" "+candidateLength+" "+rightRem);
+                int currentCount = 0;
                 while(countMax == k)
                 {
-                    ans = ans + (long)rightRem + 1l;
-                    if(nums[i] == max)
+                    currentCount++;
+                    if(nums[start] == max)
                     {
                         countMax--;
                     }
-                    i++;
+                    start++;
                 }
+                ans += currentCount*1L*(n - end + 1);
+                // System.out.println((currentCount*(n - end))+" for i: "+start);
             }
         }
+
         if(countMax == k)
         {
-            int candidateLength = j-i;
-            int rightRem = nums.length-candidateLength-i;
-            // System.out.println(i+" "+candidateLength+" "+rightRem);
+            int currentCount = 0;
             while(countMax == k)
             {
-                ans = ans + (long)rightRem + 1l;
-                if(nums[i] == max)
+                currentCount++;
+                if(nums[start] == max)
                 {
                     countMax--;
                 }
-                i++;
+                start++;
             }
+            ans += currentCount;
         }
         return ans;
     }
