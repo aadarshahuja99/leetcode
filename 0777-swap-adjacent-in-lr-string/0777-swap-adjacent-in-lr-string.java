5ae5c23073e69dd256598bea18c5e9928995987d
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-05 06:44:00 +0530

    Time: 5 ms (77.01%), Space: 43.4 MB (78.74%) - LeetHub

---
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean canTransform(String start, String target) {
+        // 2 pointers. Intuition:
+        // 1. L can only move to the left. R can only move to the Right
+        // 2. Every L in source should come after or at the same position as the last L in target (so that we can move the source L to the left)
+        // 3. Evert R in source should come before or at the same position as the last R in target (so that we can move the source L to the left)
+        // 4. L and R can not cross eachother
+        int sourcePointer = 0;
+        int targetPointer = 0;
+        int n = start.length();
+        while(sourcePointer < n && targetPointer < n)
+        {
+            if(start.charAt(sourcePointer) == 'X')
+            {
+                sourcePointer++;
+                continue;
+            }
+            if(target.charAt(targetPointer) == 'X')
+            {
+                targetPointer++;
+                continue;
+            }
+            if(start.charAt(sourcePointer) != target.charAt(targetPointer))
+            {
+                return false;
+            }
+            if(start.charAt(sourcePointer) == 'L' && sourcePointer < targetPointer)
+            {
+                return false;
+            }
+            if(start.charAt(sourcePointer) == 'R' && sourcePointer > targetPointer)
+            {
+                return false;
+            }
+            sourcePointer++;
+            targetPointer++;
+        }
+        while(sourcePointer < n && start.charAt(sourcePointer) == 'X')
+        {
+            sourcePointer++;
+        }
+        while(targetPointer < n && target.charAt(targetPointer) == 'X')
+        {
+            targetPointer++;
+        }
+        return sourcePointer == n && targetPointer == n;
+    }
+}
