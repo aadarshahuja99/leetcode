0825880b440b3fbf8fe0b196de03e31ab350bf89
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-06 15:17:19 +0530

    Time: 136 ms (16.9%), Space: 59.2 MB (64.79%) - LeetHub

---
+++ 1943-describe-the-painting/1943-describe-the-painting.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.Map.Entry;
+class Solution {
+    public List<List<Long>> splitPainting(int[][] segments) {
+        TreeMap<Integer, Long> map = new TreeMap<>();
+
+        for(int segment[]: segments) {
+            map.put(segment[0], map.getOrDefault(segment[0], 0L) + segment[2]);
+            map.put(segment[1], map.getOrDefault(segment[1], 0L) - segment[2]);
+        }
+
+        List<List<Long>> result = new ArrayList<>();
+
+        int prev = 0;
+        long sum = 0;
+
+        for(int key: map.keySet()) {
+            // System.out.println(prev+" "+key+" "+sum);
+            if(sum != 0) { // Ignore the unpainted interval
+                result.add(Arrays.asList((long)prev, (long)key, sum)); // Add the interval
+            }
+
+            sum += map.get(key);
+            prev = key;
+        }
+
+        return result;
+    }
+}
