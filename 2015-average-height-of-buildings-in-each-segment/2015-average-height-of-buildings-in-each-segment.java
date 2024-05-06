b6da3334bab376b08a69e144489d36ac3d823dac
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-06 23:23:03 +0530

    Time: 188 ms (33.33%), Space: 118.3 MB (66.67%) - LeetHub

---
+++ 2015-average-height-of-buildings-in-each-segment/2015-average-height-of-buildings-in-each-segment.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int[][] averageHeightOfBuildings(int[][] buildings) {
+        TreeMap<Integer,int[]> map = new TreeMap<>();
+        for(int[] building : buildings)
+        {
+            if(map.containsKey(building[0]))
+            {
+                int[] existing = map.get(building[0]);
+                existing[0] += building[2];
+                existing[1] += 1;
+                map.put(building[0], existing);
+            }
+            else
+            {
+                map.put(building[0], new int[] { building[2], 1 });
+            }
+            if(map.containsKey(building[1]))
+            {
+                int[] existing = map.get(building[1]);
+                existing[0] -= building[2];
+                existing[1] -= 1;
+                map.put(building[1], existing);
+            }
+            else
+            {
+                map.put(building[1], new int[] { -building[2], -1 });
+            }
+        }
+
+        int runningSum = 0;
+        int runningCount = 0;
+        int prev = -1;
+        int lastAverage = -1;
+        ArrayList<int[]> intermediate = new ArrayList<>();
+        for(int key : map.keySet())
+        {
+            int[] current = map.get(key);
+            runningSum += current[0];
+            runningCount += current[1];
+            int currentAverage = -1;
+            if(runningCount > 0)
+            {
+                currentAverage = runningSum/runningCount;
+            }
+            // System.out.println(currentAverage+" "+lastAverage+" "+key+" "+runningSum+" "+runningCount);
+            if(currentAverage == lastAverage)
+            {
+                continue;
+            }
+            if(prev >= 0 && lastAverage >= 0)
+            {
+                intermediate.add(new int[] { prev, key, lastAverage });
+            }
+            prev = key;
+            lastAverage = currentAverage;
+        }
+        int size = intermediate.size();
+        int[][] ans = new int[size][3];
+        int idx = 0;
+        for(int[] list : intermediate)
+        {
+            ans[idx] = list;
+            idx++;
+        }
+        return ans;
+    }
+}
