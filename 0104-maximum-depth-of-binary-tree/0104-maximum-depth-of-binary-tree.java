8de1236b324b6ad64f6564056c4560fefedeceb1
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-10 16:07:14 +0530

    Time: 1 ms (11.3%), Space: 42.6 MB (42.9%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        return dfs(root);
+        return depthUsingMorris(root);
     }
     private int dfs(TreeNode current)
     {
@@ -26,5 +26,52 @@ class Solution {
         int leftTreeLength = dfs(current.left);
         int rightTreeLength = dfs(current.right);
         return 1 + Math.max(leftTreeLength, rightTreeLength);
+    }
+
+    private int depthUsingMorris(TreeNode root)
+    {
+        TreeNode current = root;
+        if(root == null)
+        {
+            return 0;
+        }
+        int currentDepth = 0;
+        int ans = 0;
+        while(current != null)
+        {
+            if(current.left != null)
+            {
+                int heightOfTheLeftSubtree = 1;
+                TreeNode leftChild = current.left;
+                while(leftChild.right != null && leftChild.right != current)
+                {
+                    leftChild = leftChild.right;
+                    heightOfTheLeftSubtree++;
+                }
+                if(leftChild.right == current)
+                {
+                    leftChild.right = null;
+                    currentDepth -= heightOfTheLeftSubtree + 1;
+                    ans = Math.max(ans, currentDepth);
+                    current = current.right;
+                    currentDepth++;
+                }
+                else
+                {
+                    leftChild.right = current;
+                    current = current.left;
+                    currentDepth++;
+                    ans = Math.max(ans, currentDepth);
+                    // System.out.println(currentDepth+" = depth at "+current.val);
+                }
+            }
+            else
+            {
+                current = current.right;
+                ans = Math.max(ans, currentDepth);
+                currentDepth++;
+            }
+        }
+        return ans + 1;
     }
 }
