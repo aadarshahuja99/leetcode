cd39cece1c29c6dd3bf27b23370ce95141412107
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-10 08:41:35 +0530

    Time: 1 ms (58.46%), Space: 41.1 MB (91.85%) - LeetHub

---
+++ 0362-design-hit-counter/0362-design-hit-counter.java
@@ -0,0 +1,38 @@
+class HitCounter {
+    Queue<Integer> timeStamps;
+    final static int MAX_QUEUE_SIZE = 300;
+    public HitCounter() {
+        timeStamps = new LinkedList<>();
+    }
+    
+    public void hit(int timestamp) {
+        while(!timeStamps.isEmpty() && (timestamp - timeStamps.peek()) >= 300)
+        {
+            timeStamps.poll();
+        }
+        if(timeStamps.size() == MAX_QUEUE_SIZE)
+        {
+            timeStamps.poll();
+        }
+        timeStamps.add(timestamp);
+    }
+    
+    public int getHits(int timestamp) {
+        if(timeStamps.isEmpty())
+        {
+            return 0;
+        }
+        while(!timeStamps.isEmpty() && (timestamp - timeStamps.peek()) >= MAX_QUEUE_SIZE)
+        {
+            timeStamps.poll();
+        }
+        return timeStamps.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
