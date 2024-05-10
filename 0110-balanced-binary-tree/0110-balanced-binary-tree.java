9b96ffc02425a838cd5fc7cbd19c0a845ba3d9c3
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-10 18:41:53 +0530

    Time: 10 ms (5.28%), Space: 44.8 MB (6.72%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,7 +15,11 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return (check(root)).getValue();
+        if(root == null)
+        {
+            return true;
+        }
+        return checkForBalancedTree(root);
     }
     private Pair<Integer,Boolean> check(TreeNode root)
     {
@@ -29,5 +33,51 @@ class Solution {
 
         int heightDifference = Math.abs(left.getKey() - right.getKey());
         return new Pair<Integer,Boolean>(1 + Math.max(left.getKey(), right.getKey()), (heightDifference <= 1) && left.getValue() && right.getValue());
+    }
+
+    private boolean checkForBalancedTree(TreeNode root)
+    {
+        TreeNode current = root;
+        HashSet<TreeNode> visited = new HashSet<>();
+        HashMap<TreeNode,Integer> heights = new HashMap<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(current);
+
+        while(!stack.isEmpty())
+        {
+            visited.add(stack.peek());
+            heights.put(current, 1);
+            if(stack.peek().left != null && !visited.contains(stack.peek().left))
+            {
+                heights.put(stack.peek().left,1);
+                stack.push(stack.peek().left);
+                continue;
+            }
+            if(stack.peek().right != null && !visited.contains(stack.peek().right))
+            {
+                heights.put(stack.peek().right,1);
+                stack.push(stack.peek().right);
+                continue;
+            }
+            int leftHeight = 0;
+            int rightHeight = 0;
+            if(stack.peek().left != null)
+            {
+                leftHeight = heights.get(stack.peek().left);
+            }
+            if(stack.peek().right != null)
+            {
+                rightHeight = heights.get(stack.peek().right);
+            }
+            // System.out.println(leftHeight+" "+rightHeight+" "+stack.peek().val);
+            if(Math.abs(leftHeight - rightHeight) > 1)
+            {
+                return false;
+            }
+            heights.put(stack.peek(), heights.get(stack.peek()) + Math.max(leftHeight, rightHeight));
+            stack.pop();
+        }
+        return true;
     }
 }
