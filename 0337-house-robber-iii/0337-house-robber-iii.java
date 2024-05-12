b87e23c98f2fdeb8672ccab9b2f60925319ebe5e
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-12 17:55:06 +0530

    Time: 6 ms (7.24%), Space: 44.6 MB (45.35%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        HashMap<TreeNode,HashMap<Integer,Integer>> cache = new HashMap<>();
+        return getMaxMoney(root, 1, cache);
+    }
+    private int getMaxMoney(TreeNode root, int canRob, HashMap<TreeNode,HashMap<Integer,Integer>> cache)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        if(cache.containsKey(root) && cache.get(root).containsKey(canRob))
+        {
+            return cache.get(root).get(canRob);
+        }
+        if(!cache.containsKey(root))
+        {
+            cache.put(root, new HashMap<>());
+        }
+        if(canRob == 1)
+        {
+            int maxAns = Math.max(root.val + getMaxMoney(root.left, 0, cache) + getMaxMoney(root.right, 0, cache),
+            getMaxMoney(root.left, 1, cache) + getMaxMoney(root.right, 1, cache));
+            cache.get(root).put(canRob, maxAns);
+            return cache.get(root).get(canRob);
+        }
+        int ans = getMaxMoney(root.left, 1, cache) + getMaxMoney(root.right, 1, cache);
+        cache.get(root).put(canRob, ans);
+        return cache.get(root).get(canRob);
+    }
+}
