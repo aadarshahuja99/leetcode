654d4682ddc91bac75033ffa7b243cdba0241947
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-17 20:32:27 +0530

    Time: 86 ms (42.78%), Space: 71 MB (49.44%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -0,0 +1,23 @@
+class Solution {
+    int mod = 1000000007;
+    public int numberOfWays(int startPos, int endPos, int k) {
+        int[][] cache = new int[3001][k+1];
+        for(int[] row : cache)
+        {
+            Arrays.fill(row, -1);
+        }
+        return getAns(startPos + k, k, endPos, k, cache);
+    }
+    private int getAns(int currentPos, int steps, int end, int k, int[][] cache)
+    {
+        if(steps == 0)
+        {
+            return currentPos == end + k ? 1 : 0;
+        }
+        if(cache[currentPos][steps] != -1)
+        {
+            return cache[currentPos][steps];
+        }
+        return cache[currentPos][steps] = (getAns(currentPos + 1, steps - 1, end, k, cache)%mod + getAns(currentPos - 1, steps - 1, end, k, cache)%mod)%mod;
+    }
+}
