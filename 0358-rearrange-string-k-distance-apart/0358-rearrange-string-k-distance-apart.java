4d376d3dfb3b85c4d397e5261989c87ffaa8d7cb
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-21 20:04:30 +0530

    Time: 14 ms (72.58%), Space: 44.3 MB (94.31%) - LeetHub

---
+++ 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.java
@@ -0,0 +1,59 @@
+class Solution {
+    public String rearrangeString(String s, int k) {
+        int[][] counts = new int[26][2];
+        for(int i=0; i<26; i++)
+        {
+            counts[i][0] = i;
+        }
+        for(char c : s.toCharArray())
+        {
+            counts[c-97][1]++;
+        }
+        if(k == 0)
+        {
+            String ans = "";
+            for(int i=0; i<26; i++)
+            {
+                if(counts[i][1] > 0)
+                {
+                    char c = (char)(i + 97);
+                    for(int j=0; j<counts[i][1]; j++)
+                    {
+                        ans += c;
+                    }
+                }
+            }
+            return ans;
+        }
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> {
+            return b[1] - a[1];
+        });
+        for(int i=0; i<26; i++)
+        {
+            if(counts[i][1] > 0)
+            {
+                maxHeap.add(counts[i]);
+            }
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        while(maxHeap.size() > 0)
+        {
+            int[] top = maxHeap.poll();
+            char c = (char)(97 + top[0]);
+            top[1]--;
+            // System.out.println(c+" "+top[1]);
+            sb.append(c);
+            queue.add(top);
+            if(queue.size() == k)
+            {
+                var queueTop = queue.poll();
+                if(queueTop[1] > 0)
+                {
+                    maxHeap.add(queueTop);
+                }
+            }
+        }
+        return sb.length() == s.length() ? sb.toString() : "";
+    }
+}
