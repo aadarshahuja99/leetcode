d6f5fd0177245545f94c2832816678c8233f5e9f
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-22 18:12:21 +0530

    Time: 27 ms (33.54%), Space: 49.6 MB (80.6%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -0,0 +1,43 @@
+class Solution {
+    int[] pre;
+    int total;
+    public Solution(int[] w) {
+        int n = w.length;
+        pre = new int[n];
+        total = 0;
+        pre[0] = w[0];
+        for(int i=1; i<n; i++)
+        {
+            pre[i] = pre[i-1] + w[i];
+        }
+        total = pre[n-1];
+    }
+    
+    public int pickIndex() {
+        Random r = new Random();
+        int idx = r.nextInt(1, total+1);
+        int start = 0;
+        int end = pre.length;
+        int ceil = -1;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            if(idx <= pre[mid])
+            {
+                ceil = mid;
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return ceil;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
