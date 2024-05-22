62ab35c8a50a95fd17a8e6ed7e774cc72fe3059c
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-22 11:03:04 +0530

    Time: 100 ms (43.31%), Space: 56.6 MB (47.77%) - LeetHub

---
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int lengthOfLIS(int[] nums, int k) {
+        int max = 0;
+        for(int num : nums)
+        {
+            max = Math.max(max, num);
+        }
+        int ans = 0;
+        SegmentTree tree = new SegmentTree(max+1);
+        for(int num : nums)
+        {
+            int length = tree.query(Math.max(0, num-k), num - 1);
+            tree.update(length+1, num);
+            ans = Math.max(length+1, ans);
+        }
+        return ans;
+    }
+    class SegmentTree
+    {
+        int[] tree;
+        int size;
+        public SegmentTree(int n)
+        {
+            size = n;
+            tree = new int[4*100001];
+        }
+        public void update(int val, int idx)
+        {
+            updateHelper(val, idx, 0, 0, 100000);
+        }
+        private void updateHelper(int val, int idx, int treeIndex, int ss, int se)
+        {
+            if(ss == se)
+            {
+                tree[treeIndex] = val;
+                return;
+            }
+            int mid = ss + (se - ss)/2;
+            if(mid >= idx)
+            {
+                updateHelper(val, idx, 2*treeIndex+1, ss, mid);
+            }
+            else
+            {
+                updateHelper(val, idx, 2*treeIndex+2, mid+1, se);
+            }
+            tree[treeIndex] = Math.max(tree[2*treeIndex+1], tree[2*treeIndex+2]);
+        }
+        public int query(int l, int r)
+        {
+            return queryHelper(0, 100000, l, r, 0);
+        }
+        public int queryHelper(int ss, int se, int l, int r, int treeIndex)
+        {
+            if(ss > r || se < l)
+            {
+                return Integer.MIN_VALUE;
+            }
+            if(ss >= l && se <= r)
+            {
+                return tree[treeIndex];
+            }
+            int mid = ss + (se - ss)/2;
+            int left = queryHelper(ss, mid, l, r, 2*treeIndex+1);
+            int right = queryHelper(mid+1, se, l, r, 2*treeIndex+2);
+            return Math.max(left, right);
+        }
+    }
+}
