98c942c20f3ecf5b7ab6332c339542c380a5bb48
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-24 06:18:56 +0530

    Time: 137 ms (24.39%), Space: 60.9 MB (12.2%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.java
@@ -3,7 +3,7 @@ class Solution {
         // each element 'num' of the array can be assigned a range: num-k to num+k.
         // We have n intervals now, we have to find the largest intersection of these n intervals. i.e a value from min nums[i] - k to max nums[i] + k
         // DP can not be applied to problems where we are manipulating the elements of the initial array
-        int[] hash = new int[4*100000+2];
+        int[] hash = new int[3*100000+2];
         for(int num : nums)
         {
             hash[num-k+100000]++;
