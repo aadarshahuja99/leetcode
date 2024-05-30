9d8f79587aa5675365192f954869b6386f1d050d
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-05-30 20:49:09 +0530

    Time: 127 ms (5.12%), Space: 56.6 MB (57.23%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        Arrays.sort(quantities);
+        long totalProducts = 0;
+        int numberOfProducts = quantities.length;
+        for(int i=0; i<numberOfProducts; i++)
+        {
+            totalProducts += 1L*quantities[i];
+        }
+        long start = 1;
+        long end  = totalProducts;
+        long ans = -1;
+        // System.out.println(quantities.length + " ");
+        while(start <= end)
+        {
+            long mid = start + (end-start)/2; 
+            if(check(mid, quantities, n, numberOfProducts))
+            {
+                end = mid-1;
+                ans = mid;
+            }
+            else
+            {
+                start = mid+1;
+            }
+        }
+        return (int)ans;
+    }
+    private boolean check(long guess, int[] quantities, int n, int numberOfProducts)
+    {
+        long numberOfStoresNeeded = 0;
+        for(int i=0; i<numberOfProducts; i++)
+        {
+            numberOfStoresNeeded += (long)(quantities[i]/guess);
+            if((quantities[i]%guess) > 0)
+            {
+                numberOfStoresNeeded++;
+            }
+        }
+        // System.out.println(guess+" "+(numberOfStoresNeeded <= n));
+        return numberOfStoresNeeded <= n;
+    }
+}
