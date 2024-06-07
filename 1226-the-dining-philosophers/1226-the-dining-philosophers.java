e718d4c222f9eabf95299df5d378cb735c622b6a
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-07 06:45:57 +0530

    Time: 12 ms (96.83%), Space: 45.5 MB (61.1%) - LeetHub

---
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.java
@@ -0,0 +1,46 @@
+class DiningPhilosophers {
+    boolean[] forkStatus;
+    final Object lock;
+    public DiningPhilosophers() {
+        forkStatus = new boolean[5];
+        Arrays.fill(forkStatus, true);
+        lock = new Object();
+    }
+
+    // call the run() method of any runnable to execute its code
+    public void wantsToEat(int philosopher,
+                           Runnable pickLeftFork,
+                           Runnable pickRightFork,
+                           Runnable eat,
+                           Runnable putLeftFork,
+                           Runnable putRightFork) throws InterruptedException {
+        int leftForkId = philosopher;
+        int rightForkId = (philosopher + 1)%5;
+        synchronized(lock)
+        {
+            while(!forkStatus[leftForkId])
+            {
+                lock.wait();
+            }
+            forkStatus[leftForkId] = false;
+            pickLeftFork.run();
+            lock.notifyAll();
+            while(!forkStatus[rightForkId])
+            {
+                lock.wait();
+            }
+            forkStatus[rightForkId] = false;
+            pickRightFork.run();
+            
+            eat.run();
+
+            forkStatus[rightForkId] = true;
+            putRightFork.run();
+            
+            forkStatus[leftForkId] = true;
+            putLeftFork.run();
+            
+            lock.notifyAll();
+        }
+    }
+}
