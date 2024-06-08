8cd95e61fd9a8cf8e8b5872505c5e33ad325eae6
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-08 09:18:59 +0530

    Time: 9 ms (99.42%), Space: 42.8 MB (49.71%) - LeetHub

---
+++ 1188-design-bounded-blocking-queue/1188-design-bounded-blocking-queue.java
@@ -0,0 +1,38 @@
+class BoundedBlockingQueue {
+    Queue<Integer> queue;
+    int limit;
+    public BoundedBlockingQueue(int capacity) {
+        queue = new LinkedList<>();
+        limit = capacity;
+    }
+    
+    public void enqueue(int element) throws InterruptedException {
+        synchronized(this)
+        {
+            while(queue.size() == limit)
+            {
+                this.wait();
+            }
+            queue.add(element);
+            notifyAll();
+        }
+    }
+    
+    public int dequeue() throws InterruptedException {
+        int top = -1;
+        synchronized(this)
+        {
+            while(queue.isEmpty())
+            {
+                this.wait();
+            }
+            top = queue.poll();
+            notifyAll();
+        }
+        return top;
+    }
+    
+    public int size() {
+        return queue.size();
+    }
+}
