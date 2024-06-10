62af897a63f273d62bae5343f5c8f18b81388b2d
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-10 21:34:27 +0530

    Time: 636 ms (98.29%), Space: 0B (100%) - LeetHub

--- 1555-bank-account-summary/1555-bank-account-summary.sql
+++ 1555-bank-account-summary/1555-bank-account-summary.sql
@@ -4,14 +4,10 @@ with out_cte as (
 ),
 in_cte as (
     select paid_to as user_id, IFNULL(SUM(amount), 0) as total_in from Transactions group by paid_to order by paid_to
-),
-updated_users as (
-    select * from users order by users.user_id
 )
-
 -- select * from out_cte;
 
 select u.user_id as user_id, u.user_name as user_name, u.credit + IFNULL(total_in,0) - IFNULL(total_out, 0) as credit,
 CASE WHEN u.credit + IFNULL(total_in,0) - IFNULL(total_out, 0) >= 0 THEN 'No' ELSE 'Yes' END as credit_limit_breached
-from updated_users u left join out_cte on out_cte.user_id = u.user_id
+from users u left join out_cte on out_cte.user_id = u.user_id
 left join in_cte on in_cte.user_id = u.user_id;
