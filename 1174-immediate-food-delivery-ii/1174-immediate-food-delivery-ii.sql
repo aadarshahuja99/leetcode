04196b578036c7fc73d047b83b51f1e35ff0be9c
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-10 18:48:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+WITH cte as (
+    select customer_id, MIN(order_date) as min_date from Delivery group by customer_id
+),
+cte_count as (
+    select SUM(CASE WHEN min_date = customer_pref_delivery_date THEN 1 ELSE 0 END) as valid_count from
+    Delivery inner join cte on cte.customer_id = Delivery.customer_id
+)
+select ROUND((cte_count.valid_count/(select count(distinct customer_id) from Delivery))*100, 3) as immediate_percentage from cte_count;
