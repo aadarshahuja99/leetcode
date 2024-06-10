fb8e8bc3c8f722c01f22c8a14e3ce2f7fc6da284
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-10 13:28:13 +0530

    Time: 593 ms (98.05%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+WITH updated_users as (
+    select * from Signups order by user_id 
+),
+updated_con as (
+    select * from confirmations order by user_id
+)
+select u.user_id as user_id, ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 2) as confirmation_rate from
+updated_users u left join updated_con c on u.user_id = c.user_id group by u.user_id;
+-- select user_id, SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) as accepted_count from
+--      Confirmations group by user_id order by user_id
