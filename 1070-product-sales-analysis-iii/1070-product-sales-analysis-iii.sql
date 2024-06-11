bff5a3f9f108910c1cf39abdc9acc26589dec70b
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-11 11:43:52 +0530

    Time: 1192 ms (94.04%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 with cte as (
-    select distinct product_id, MIN(year) OVER (PARTITION BY product_id ORDER BY year) as first_year from sales
+    select distinct product_id, MIN(year) OVER (PARTITION BY product_id) as first_year from sales
 )
 select p.product_id, p.year as first_year, p.quantity, price from sales p, cte where p.year = cte.first_year and p.product_id = cte.product_id;
 
