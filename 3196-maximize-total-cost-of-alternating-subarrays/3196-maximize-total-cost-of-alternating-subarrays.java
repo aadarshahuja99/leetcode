ce680435c39a0ed2add01669ba8e7d92bf239ad8
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-06-28 09:53:10 +0530

    Time: 47 ms (24.88%), Space: 80.8 MB (31.4%) - LeetHub

---
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long maximumTotalCost(int[] nums) {
+        int n = nums.length;
+        long[][] cache = new long[n][2];
+        for(long[] row : cache)
+        {
+            Arrays.fill(row, Long.MIN_VALUE);
+        }
+        return getAns(0, 0, nums, cache);
+    }
+    private long getAns(int currentIndex, int lastSign, int[] nums, long[][] cache)
+    {
+        if(currentIndex == nums.length)
+        {
+            return 0l;
+        }
+        if(cache[currentIndex][lastSign] != Long.MIN_VALUE)
+        {
+            return cache[currentIndex][lastSign];
+        }
+        long startNew = nums[currentIndex]*1l + getAns(currentIndex+1, 1, nums, cache);
+        long continued = 0l;
+        if(lastSign == 1)
+        {
+            continued = -nums[currentIndex]*1l + getAns(currentIndex+1, 0, nums, cache);
+        }
+        else
+        {
+            continued = nums[currentIndex]*1l + getAns(currentIndex+1, 1, nums, cache);
+        }
+        return cache[currentIndex][lastSign] = Math.max(startNew, continued);
+    }
+}
