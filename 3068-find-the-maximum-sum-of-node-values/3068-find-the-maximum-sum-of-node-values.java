9f4f36eac6148cd040b16cb4cffac155ba150a2f
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-07-07 16:47:36 +0530

    Time: 19 ms (7.85%), Space: 59.5 MB (5.44%) - LeetHub

---
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long[][] cache = new long[nums.length][2];
+        for(long[] row : cache)
+        {
+            Arrays.fill(row, -1l);
+        }
+        return getAns(0, 1, k, nums, cache);
+    }
+    private long getAns(int current, int isEven, int k, int[] nums, long[][] cache)
+    {
+        if(current == nums.length)
+        {
+            if(isEven == 0)
+            {
+                return Long.MIN_VALUE;
+            }
+            return 0l;
+        }
+        if(cache[current][isEven] != -1)
+        {
+            return cache[current][isEven];
+        }
+        long take = (1l*nums[current]^k) + getAns(current+1, isEven == 1 ? 0 : 1, k, nums, cache);
+        long notTake = (1l*nums[current]) + getAns(current+1, isEven, k, nums, cache);
+        // System.out.println(take+" "+notTake+" "+current+" "+isEven+", "+(nums[current]^k)+" "+nums[current]);
+        return cache[current][isEven] = Math.max(take, notTake);
+    }
+}
