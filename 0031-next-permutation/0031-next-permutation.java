b86c0096a6d568ce1fb1a3e10d595bbb3c9f3a5e
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-07-11 15:40:42 +0530

    Time: 1 ms (20.78%), Space: 43.2 MB (8.81%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int peakIdx = -1;
+        for(int i=nums.length-2; i>=0; i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                peakIdx = i;
+                break;
+            }
+        }
+        if(peakIdx == -1)
+        {
+            Arrays.sort(nums);
+            return;
+        }
+        int nextGreater = nums.length;
+        for(int i=nums.length-1; i>peakIdx; i--)
+        {
+            if(nums[i] > nums[peakIdx])
+            {
+                if(nextGreater == nums.length || nums[i] < nums[nextGreater])
+                {
+                    nextGreater = i;
+                }
+            }
+        }
+        int temp = nums[nextGreater];
+        nums[nextGreater] = nums[peakIdx];
+        nums[peakIdx] = temp;
+        int end = nums.length-1;
+        int start = peakIdx+1;
+        while(start <= end)
+        {
+            int t = nums[start];
+            nums[start] = nums[end];
+            nums[end] = t;
+            start++;
+            end--;
+        }
+    }
+}
