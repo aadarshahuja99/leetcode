2fcf874522efd224328e2ca8379d18d4540c9885
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-07-12 15:02:45 +0530

    Time: 17 ms (45.35%), Space: 46.6 MB (56.42%) - LeetHub

---
+++ 1245-tree-diameter/1245-tree-diameter.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int treeDiameter(int[][] edges) {
+        int n = edges.length + 1;
+        // Build the graph with the adjacency list
+        ArrayList<Set<Integer>> neighbors = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            neighbors.add(new HashSet<Integer>());
+
+        for (int[] edge : edges) {
+            Integer start = edge[0], end = edge[1];
+            neighbors.get(start).add(end);
+            neighbors.get(end).add(start);
+        }
+
+        // Initialize the first layer of leaves
+        ArrayList<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (neighbors.get(i).size() == 1)
+                leaves.add(i);
+
+        // Trim the leaves until reaching the centroids
+        int remainingNodes = n;
+        int distance = 0;
+        while (remainingNodes > 2) {
+            remainingNodes -= leaves.size();
+            distance++;
+            ArrayList<Integer> newLeaves = new ArrayList<>();
+
+            // remove the current leaves along with the edges
+            for (Integer leaf : leaves) {
+                // the only neighbor left for the leaf node
+                Integer neighbor = neighbors.get(leaf).iterator().next();
+                // remove the edge along with the leaf node
+                neighbors.get(neighbor).remove(leaf);
+                if (neighbors.get(neighbor).size() == 1)
+                    newLeaves.add(neighbor);
+            }
+
+            // prepare for the next round
+            leaves = newLeaves;
+        }
+        if(remainingNodes == 2)
+        {
+            return 2*distance + 1;
+        }
+        // The remaining nodes are the centroids of the graph
+        return 2*distance;
+    }
+}
