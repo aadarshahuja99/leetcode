b5e54db70a7b5751585e9926c0a756645b0fdda6
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-07-12 20:46:38 +0530

    Time: 39 ms (85.77%), Space: 112.4 MB (43.08%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        Arrays.sort(meetings, new Comparator<int[]>() {
-            public int compare(int[] m1, int[] m2)
-            {
-                return m1[2] - m2[2];
-            }
+        Arrays.sort(meetings, (a,b) -> {
+            return a[2] - b[2];
         });
-        int i=0;
         DisjointSet ds = new DisjointSet(n);
         ds.union(0,firstPerson);
-        while(i<meetings.length)
+        int m=meetings.length;
+        for(int i=0; i<m;)
         {
-            HashMap<Integer,HashSet<Integer>> adj = new HashMap<>();
             int j=i;
-            int meetingTime = meetings[j][2];
-            HashSet<Integer> set = new HashSet<>();
-            while(j<meetings.length-1 && meetings[j][2] == meetings[j+1][2])
+            while(j < m && meetings[j][2] == meetings[i][2])
             {
-                set.add(meetings[j][1]);
-                set.add(meetings[j][0]);
                 ds.union(meetings[j][0], meetings[j][1]);
                 j++;
             }
-            set.add(meetings[j][1]);
-            set.add(meetings[j][0]);
-            ds.union(meetings[j][0], meetings[j][1]);
-            for(int person : set)
+            int par = ds.findParent(0);
+            for(int k=i; k<j; k++)
             {
-                ds.resetParents(person);
+                if(ds.findParent(meetings[k][0]) != par)
+                {
+                    ds.reset(meetings[k][0], meetings[k][1]);
+                }
             }
-            i=j+1;
+            i=j;
         }
         return ds.getAns();
     }
@@ -37,18 +30,15 @@ class Solution {
     class DisjointSet
     {
         int[] parent;
-        int[] size;
         int n;
         public DisjointSet(int people)
         {
             n = people;
             parent = new int[n];
-            size = new int[n];
             for(int i=0; i<n; i++)
             {
                 parent[i] = i;
             }
-            Arrays.fill(size,1);
         }
         public int findParent(int u)
         {
@@ -60,50 +50,20 @@ class Solution {
         }
         public void union(int u, int v)
         {
-            int u_parent = findParent(u);
-            int v_parent = findParent(v);
-            if(u_parent == 0 && v_parent == 0)
-            {
-                return;
-            }
-            if(u_parent == 0)
-            {
-                parent[v_parent] = 0;
-                size[0] += size[v_parent];
-                return;
-            }
-            if(v_parent == 0)
-            {
-                parent[u_parent] = 0;
-                size[0] += size[u_parent];
-                return;
-            }
-            if(size[u_parent] >= size[v_parent])
-            {
-                parent[v_parent] = u_parent;
-                size[u_parent] += size[v_parent];
-            }
-            else
-            {
-                parent[u_parent] = v_parent;
-                size[v_parent] += size[u_parent];
-            }
+            parent[findParent(u)] = findParent(v);
         }
-        public void resetParents(int i)
+        public void reset(int u, int v)
         {
-            int current_parent = findParent(i);
-            if(current_parent != 0)
-            {
-                parent[i] = i;
-                size[i] = 1;
-            }
+            parent[u] = u;
+            parent[v] = v;
         }
-        public ArrayList<Integer> getAns()
+        public List<Integer> getAns()
         {
-            var ans = new ArrayList<Integer>();
+            List<Integer> ans = new ArrayList<>();
+            int par = findParent(0);
             for(int i=0; i<n; i++)
             {
-                if(findParent(i) == 0)
+                if(findParent(i) == par)
                 {
                     ans.add(i);
                 }
