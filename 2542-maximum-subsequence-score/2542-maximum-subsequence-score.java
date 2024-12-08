c263693ddf5123f079261a42f850886c470e1521
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-12-08 20:02:30 +0530

    Time: 81 ms (32.29%), Space: 59.9 MB (94.81%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -0,0 +1,28 @@
+class Solution {
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        long sum = 0;
+        int n = nums1.length;
+        int[][] combined = new int[n][3];
+        for(int i=0; i<n; i++)
+        {
+            combined[i] = new int[] { nums1[i], nums2[i], i };
+        }
+        Arrays.sort(combined, (a,b) -> {
+            return b[1] - a[1];
+        });
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<k; i++)
+        {
+            sum += combined[i][0];
+            pq.add(combined[i][0]);
+        }
+        long ans = sum*1l*combined[k-1][1];
+        for(int i=k; i<n; i++)
+        {
+            sum += (combined[i][0] - pq.poll());
+            ans = Math.max(ans, 1l*sum*combined[i][1]);
+            pq.add(combined[i][0]);
+        }
+        return ans;
+    }
+}
