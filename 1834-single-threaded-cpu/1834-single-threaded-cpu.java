03fbe8d1d033450ffc66fa5354f51aa88bbec9d7
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2024-12-08 19:26:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -24,10 +24,6 @@ class Solution {
                 return a[1] - b[1];
             }
         });
-        // for(int[] task : indexedTasks)
-        // {
-        //     System.out.println(task[0]+" "+task[1]+" "+task[2]);
-        // }
         int minTime = indexedTasks[0][0];
         idx = 0;
         for(int i=0; i<numTasks; i++)
@@ -46,23 +42,14 @@ class Solution {
         while(pq.size() > 0)
         {
             int[] top = pq.poll();
-            // System.out.println("polled: "+top[0]+" "+top[1]+" "+top[2]+" currentTime: "+currentTime);
             ans[processed] = top[2];
             currentTime = top[1] + currentTime;
-            if(pq.size() == 0 && idx < numTasks && indexedTasks[idx][0] > currentTime)
-            {
-                currentTime = indexedTasks[idx][0];
-                System.out.println(indexedTasks[idx][2]+" "+currentTime);
-                pq.add(indexedTasks[idx]);
-                idx++;
-            }
             while(idx < numTasks && indexedTasks[idx][0] <= currentTime)
             {
                 // System.out.println("adding "+indexedTasks[idx][2]+" to the pq");
                 pq.add(indexedTasks[idx]);
                 idx++;
             }
-            // System.out.println();
             processed++;
         }
         return ans;
