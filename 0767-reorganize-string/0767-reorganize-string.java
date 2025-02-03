8b3c5453310afc7f345ef341fdcff4c377964d8d
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-02-03 08:02:26 +0530

    Time: 3 ms (70.96%), Space: 41.7 MB (74.84%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,42 +1,37 @@
 class Solution {
     public String reorganizeString(String s) {
-        int[][] counts = new int[26][2];
-        for(int i=0; i<26; i++)
-        {
-            counts[i][0] = i;
-        }
-        for(char c : s.toCharArray())
-        {
-            counts[c-97][1]++;
-        }
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> {
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
             return b[1] - a[1];
         });
+        int[] last = {-1, -1};
+        int[] counts = new int[26];
+        for(char c : s.toCharArray())
+        {
+            counts[c-'a']++;
+        }
         for(int i=0; i<26; i++)
         {
-            if(counts[i][1] > 0)
+            if(counts[i] > 0)
             {
-                maxHeap.add(counts[i]);
+                pq.add(new int[] { 'a'+i, counts[i] });
             }
         }
-        Queue<int[]> queue = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
-        while(maxHeap.size() > 0)
+        while(pq.size() > 0)
         {
-            int[] top = maxHeap.poll();
-            char c = (char)(97 + top[0]);
+            int[] top = pq.poll();
+            sb.append((char)top[0]);
             top[1]--;
-            queue.add(top);
-            sb.append(c);
-            if(queue.size() == 2)
+            if(last[0] != -1 && last[1] > 0)
             {
-                var queueTop = queue.poll();
-                if(queueTop[1] > 0)
-                {
-                    maxHeap.add(queueTop);
-                }
+                pq.add(last);
             }
+            last = top;
         }
-        return sb.length() == s.length() ? sb.toString() : "";
+        if(sb.length() < s.length())
+        {
+            return "";
+        }
+        return sb.toString();
     }
 }
