55b4f4b4b0e84fbd52bc84dd368f519af1f12576
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-02-03 08:29:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.java
@@ -0,0 +1,30 @@
+class Solution {
+    int MOD = 1_000_000_007;
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] combined = new int[n][2];
+        for(int i=0; i<n; i++)
+        {
+            combined[i] = new int[] { speed[i], efficiency[i] };
+        }
+        Arrays.sort(combined, (a,b) -> {
+            return b[1] - a[1];
+        });
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long sum = 0;
+        int ans = (int)((sum*combined[k-1][1])%MOD);
+        int idx = 0;
+        for(int[] eng : combined)
+        {
+            if(minHeap.size() == k)
+            {
+                sum -= minHeap.poll();
+            }
+            sum += (eng[0]);
+            minHeap.add(eng[0]);
+            // System.out.println("sum = "+sum+" at idx = "+idx+" and eff: "+eng[1]);
+            ans = Math.max(ans, (int)((sum*eng[1])%MOD));
+            idx++;
+        }
+        return ans;
+    }
+}
