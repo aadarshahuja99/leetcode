b4c59e272d7f0a60f81225eb9db4c35b989f4260
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-02-09 09:55:40 +0530

    Time: 1 ms (98.99%), Space: 45.1 MB (42.04%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -10,6 +10,7 @@ class Solution {
         }
         while(i < n && intervals[i][0] <= newInterval[1])
         {
+            // taking a union of the existing interval with the current version of new interval
             newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
             i++;
