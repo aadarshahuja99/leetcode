ac0e9b1963c3b5c81b962d862abccb9adcb3e6df
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-02-09 09:56:22 +0530

    Time: 56 ms (32.86%), Space: 69.4 MB (30.86%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -11,26 +11,22 @@ class Solution {
                 return Long.compare(a[1],b[1]);
             }
         });
-        Stack<int[]> stack = new Stack<>();
-        stack.push(points[0]);
-        System.out.println(points[0][0] + " " + points[0][1]);
+        int currentEnd = points[0][1];
+        int ans = 1;
+        // System.out.println(points[0][0] + " " + points[0][1]);
         for(int i=1; i<points.length; i++)
         {
             int[] current = points[i];
-            if(stack.peek()[1] >= current[0])
+            if(currentEnd >= current[0])
             {
-                int lower = Math.max(current[0], stack.peek()[0]);
-                int upper = Math.min(current[1], stack.peek()[1]);
-                var top = stack.pop();
-                top[0] = lower;
-                top[1] = upper;
-                stack.push(top);
+                currentEnd = Math.min(current[1], currentEnd);
             }
             else
             {
-                stack.push(current);
+                currentEnd = current[1];
+                ans++;
             }
         }
-        return stack.size();
+        return ans;
     }
 }
