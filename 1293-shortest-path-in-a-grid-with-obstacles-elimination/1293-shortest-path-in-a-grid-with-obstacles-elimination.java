58d6ab23e40ca6227418b6d247df768e92fe59dc
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-05 16:44:48 +0530

    Time: 34 ms (31.28%), Space: 47.3 MB (28.79%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -2,64 +2,45 @@ class Solution {
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        // naming this as memo, because we are memoizing the shortest path encountered so far. This is not the same as dp.
-        // In dp, we traverse to the final state (explore the entire current path), and, then decide. Here, we look back in time and not look forward.
-        // In a way, this is greedy plus memoization
-        // int[][] memo = new int[m][n];
-        // for(int[] row : mat)
-        // {
-        //     Arrays.fill(row,Integer.MAX_VALUE);
-        // }
-        // for(int i=0; i<=k; i++)
-        // {
-        //     memo[0][0][i] = 0;
-        // }
-        Queue<int[]> queue = new LinkedList<int[]>();
-        queue.add(new int[] { 0,0,k });
-        int[] dr = new int[] { 0,1,0,-1 };
-        int[] dc = new int[] { 1,0,-1,0 };
-        int dist = 0;
-        boolean[][][] visited = new boolean[m][n][k+1];
-        visited[0][0][k] = true;
-        for(int i=0; i<=k; i++)
+        int[][] cost = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][][] v = new boolean[m][n][k+1];
+        q.add(new int[] { 0, 0, 0, 0 });
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        for(int[] row : cost)
+        { Arrays.fill(row, Integer.MAX_VALUE); }
+        cost[0][0] = 0;
+        while(q.size() > 0)
         {
-            visited[0][0][i] = true;
-        }
-        while(queue.size() > 0)
-        {
-            int size = queue.size();
-            dist++;
-            for(int i=0; i<size; i++)
+            int[] top = q.poll();
+            int r = top[0];
+            int c = top[1];
+            int currentObs = top[2];
+            int current = top[3];
+            // System.out.println(current+" "+currentObs+" for "+r+" "+c);
+            if(v[r][c][currentObs])
             {
-                var top = queue.poll();
-                int row = top[0];
-                int col = top[1];
-                int obs = top[2];
-                if(row == m-1 && col == n-1)
+                continue;
+            }
+            v[r][c][currentObs] = true;
+            cost[r][c] = current;
+            if(r == m-1 && c == n-1)
+            {
+                return current;
+            }
+            for(int[] d : dirs)
+            {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if(nr < 0 || nr == m || nc < 0 || nc == n)
                 {
-                    return dist - 1;
+                    continue;
                 }
-                for(int j=0; j<4; j++)
+                int addedCost = grid[nr][nc];
+                if(currentObs + addedCost <= k && !v[nr][nc][currentObs + addedCost])
                 {
-                    int newRow = row + dr[j];
-                    int newCol = col + dc[j];
-                    if(newRow < 0 || newRow >= m || newCol < 0 || newCol >= n)
-                    {
-                        continue;
-                    }
-                    int candidateDistance = grid[newRow][newCol];
-                    if(grid[newRow][newCol] == 1 && obs >= 1 && !visited[newRow][newCol][obs-1])
-                    {
-                        visited[newRow][newCol][obs-1] = true;
-                        // System.out.println(dist+" pushing: "+newRow+" "+newCol+" "+(obs-1));
-                        queue.add(new int[] { newRow, newCol, obs-1 });
-                    }
-                    else if(grid[newRow][newCol] == 0 && !visited[newRow][newCol][obs])
-                    {
-                        visited[newRow][newCol][obs] = true;
-                        // System.out.println(dist+" pushing: "+newRow+" "+newCol+" "+(obs));
-                        queue.add(new int[] { newRow, newCol, obs });
-                    }
+                    cost[nr][nc] = current+1;
+                    q.add(new int[] { nr, nc, addedCost + currentObs, current+1 });
                 }
             }
         }
