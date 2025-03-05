121061a4c54abbdc1940bb96a11ccdcfa80524f4
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-05 08:06:47 +0530

    Time: 1 ms (71.43%), Space: 41 MB (44.73%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,24 +1,60 @@
 class Solution {
+    private static int HALF_INT_MIN = -1073741824; // -2**30;
+
     public int divide(int dividend, int divisor) {
-        boolean isNegative = (divisor > 0 && dividend < 0) || (divisor < 0 && dividend > 0);
-        long absDividend = Math.abs(1l*dividend);
-        long absDivisor = Math.abs(1l*divisor);
-        int pow = -1;
-        while((absDivisor*(1l<<(pow+1))) <= absDividend)
-        {
-            pow++;
+        // Special case: overflow.
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
         }
-        long ans = 0;
-        // System.out.println(pow);
-        while(pow >= 0)
-        {
-            if(absDividend >= (absDivisor*(1l<<pow)))
-            {
-                absDividend -= (absDivisor*(1l<<pow));
-                ans = (ans|(1l<<pow));
+
+        /* We need to convert both numbers to negatives.
+         * Also, we count the number of negatives signs. */
+        int negatives = 2;
+        if (dividend > 0) {
+            negatives--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            negatives--;
+            divisor = -divisor;
+        }
+
+        ArrayList<Integer> doubles = new ArrayList<>();
+        ArrayList<Integer> powersOfTwo = new ArrayList<>();
+
+        /* Nothing too exciting here, we're just making a list of doubles of 1 and
+         * the divisor. This is pretty much the same as Approach 2, except we're
+         * actually storing the values this time. */
+        int powerOfTwo = -1;
+        while (divisor >= dividend) {
+            doubles.add(divisor);
+            powersOfTwo.add(powerOfTwo);
+            // Prevent needless overflows from occurring...
+            if (divisor < HALF_INT_MIN) {
+                break;
             }
-            pow--;
+            divisor += divisor;
+            powerOfTwo += powerOfTwo;
         }
-        return isNegative ? (int)Math.max(Integer.MIN_VALUE, -1l*ans) : (int)Math.min(Integer.MAX_VALUE, ans);
+
+        int quotient = 0;
+        /* Go from largest double to smallest, checking if the current double fits.
+         * into the remainder of the dividend. */
+        for (int i = doubles.size() - 1; i >= 0; i--) {
+            if (doubles.get(i) >= dividend) {
+                // If it does fit, add the current powerOfTwo to the quotient.
+                quotient += powersOfTwo.get(i);
+                // Update dividend to take into account the bit we've now removed.
+                dividend -= doubles.get(i);
+            }
+        }
+
+        /* If there was originally one negative sign, then
+         * the quotient remains negative. Otherwise, switch
+         * it to positive. */
+        if (negatives != 1) {
+            return -quotient;
+        }
+        return quotient;
     }
 }
