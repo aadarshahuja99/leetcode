b8a95bccb65eb9cfab003d7274e5a13b0b024d76
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-06 12:27:19 +0530

    Time: 9 ms (10.39%), Space: 45.6 MB (33.6%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        for(int ast : asteroids)
+        {
+            if(st.size() == 0)
+            {
+                st.push(ast);
+            }
+            else
+            {
+                if(st.peek() > 0 && ast < 0)
+                {
+                    while(st.size() > 0 && st.peek() > 0 && Math.abs(ast) > st.peek())
+                    {
+                        st.pop();
+                    }
+                    if(st.size() == 0 || st.peek() < 0)
+                    {
+                        st.push(ast);
+                    }
+                    else if(st.peek() == Math.abs(ast))
+                    {
+                        st.pop();
+                    }
+                }
+                else
+                {
+                    st.push(ast);
+                }
+            }
+        }
+        int[] ans = new int[st.size()];
+        int idx = 0;
+        while(!st.isEmpty())
+        {
+            // System.out.println("adding "+st.peek()+" to ans array before reverse ");
+            ans[idx] = st.pop();
+            idx++;
+        }
+        for(int i=0; i<ans.length/2; i++)
+        {
+            int temp = ans[i];
+            ans[i] = ans[ans.length-i-1];
+            ans[ans.length-i-1] = temp;
+        }
+        return ans;
+    }
+}
