2870838c13badc094f74e20ab96461867b716262
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-10 22:52:12 +0530

    Time: 2 ms (74.53%), Space: 45.2 MB (38.85%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         ArrayList<List<Integer>> permutations = new ArrayList<>();
-        getAllUniquePermutations(0,(int)Math.pow(2,nums.length)-1, nums, permutations, new LinkedList<Integer>(), new HashSet<Integer>());
+        getAllUniquePermutations(0,(int)Math.pow(2,nums.length)-1, nums, permutations, new LinkedList<Integer>());
         return permutations;
     }
-    private void getAllUniquePermutations(int currentIndex, int statusMask, int[] nums, ArrayList<List<Integer>> permutations, LinkedList<Integer> currentList, HashSet<Integer> visited)
+    private void getAllUniquePermutations(int currentIndex, int statusMask, int[] nums, ArrayList<List<Integer>> permutations, LinkedList<Integer> currentList)
     {
         if(currentIndex == nums.length)
         {
-            if(!visited.contains(currentList.hashCode()))
-            {
-                ArrayList<Integer> current = new ArrayList<>(currentList);
-                permutations.add(current);
-                visited.add(currentList.hashCode());
-            }
+            permutations.add(new ArrayList<>(currentList));
             return;
         }
-        for(int i=0; i<nums.length; i++)
+        int n = nums.length;
+        HashSet<Integer> visitedNumbersForCurrentChoice = new HashSet<>();
+        for(int i=0; i<n; i++)
         {
-            if((statusMask&(1<<i)) > 0)
+            if((statusMask&(1<<i)) > 0 && !visitedNumbersForCurrentChoice.contains(nums[i]))
             {
+                visitedNumbersForCurrentChoice.add(nums[i]);
                 currentList.addLast(nums[i]);
-                getAllUniquePermutations(currentIndex+1, statusMask^(1<<i), nums, permutations, currentList, visited);
+                getAllUniquePermutations(currentIndex+1, statusMask^(1<<i), nums, permutations, currentList);
                 currentList.removeLast();
             }
         }
