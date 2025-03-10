d294e014376c0c7f4d42ccc164c93eb80b242ed6
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-10 18:32:16 +0530

    Time: 10 ms (72.24%), Space: 45.1 MB (34.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,40 +4,23 @@ class Solution {
         int end = 0;
         int n = s.length();
         int[] counts = new int[26];
-        int maxIndex = 0;
+        int maxFreq = 0;
         int ans = 0;
         while(end < s.length())
         {
-            if(end - start - counts[maxIndex] <= k)
+            // consume end
+            counts[s.charAt(end) - 'A']++;
+            maxFreq = Math.max(counts[s.charAt(end) - 'A'], maxFreq);
+            end++;
+            if((end - start) - maxFreq <= k)
             {
                 ans = Math.max(ans, end - start);
-                // System.out.println("1: "+start+" "+end);
-                counts[s.charAt(end) - 'A']++;
-                if(counts[s.charAt(end) - 'A'] > counts[maxIndex])
-                {
-                    maxIndex = s.charAt(end) - 'A';
-                }
-                end++;
             }
             else
             {
                 counts[s.charAt(start) - 'A']--;
                 start++;
-                int newMaxIndex = maxIndex;
-                for(int i=0; i<26; i++)
-                {
-                    if(counts[i] > counts[newMaxIndex])
-                    {
-                        newMaxIndex = i;
-                    }
-                }
-                maxIndex = newMaxIndex;
             }
-        }
-        if(end - start - counts[maxIndex] <= k)
-        {
-            ans = Math.max(ans, end - start);
-            // System.out.println(start+" "+end);
         }
         return ans;
     }
