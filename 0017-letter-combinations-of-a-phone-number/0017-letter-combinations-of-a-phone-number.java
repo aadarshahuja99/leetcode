39021166208b97f826dbd498afe9f822fa61e0a0
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-10 23:24:59 +0530

    Time: 1 ms (57.4%), Space: 41.7 MB (97.75%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,10 +1,11 @@
 class Solution {
+    HashMap<Integer,List<Character>> numberMap = new HashMap<>();
+    ArrayList<String> answer = new ArrayList<String>();
     public List<String> letterCombinations(String digits) {
         if(digits.equals(""))
         {
             return new ArrayList<String>();
         }
-        HashMap<Integer,List<Character>> numberMap = new HashMap<>();
         numberMap.put(2, Arrays.asList('a','b','c'));
         numberMap.put(3, Arrays.asList('d','e','f'));
         numberMap.put(4, Arrays.asList('g','h','i'));
@@ -13,21 +14,22 @@ class Solution {
         numberMap.put(7, Arrays.asList('p','q','r','s'));
         numberMap.put(8, Arrays.asList('t','u','v'));
         numberMap.put(9, Arrays.asList('w','x','y','z'));
-        ArrayList<String> answer = new ArrayList<String>();
-        getAllCombinations(0, digits, "", answer, numberMap);
+        getAllCombinations(0, digits, new StringBuilder());
         return answer;
     }
-    private void getAllCombinations(int currentIndex, String digits, String currentString, ArrayList<String> answer, HashMap<Integer,List<Character>> numberMap)
+    private void getAllCombinations(int currentIndex, String digits, StringBuilder currentString)
     {
         if(currentIndex == digits.length())
         {
-            answer.add(currentString);
+            answer.add(currentString.toString());
             return;
         }
-        int currentDigit = digits.charAt(currentIndex) - 48;
+        int currentDigit = digits.charAt(currentIndex) - '0';
         for(char c : numberMap.get(currentDigit))
         {
-            getAllCombinations(currentIndex+1, digits, currentString+c, answer, numberMap);
+            currentString.append(c);
+            getAllCombinations(currentIndex+1, digits, currentString);
+            currentString.deleteCharAt(currentString.length() - 1);
         }
     }
 }
