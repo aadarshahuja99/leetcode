b61a615d8e44ac918e13b8ba3e92b0160b548d72
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-11 16:27:46 +0530

    Time: 7 ms (73.95%), Space: 45.4 MB (70.5%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -1,24 +1,25 @@
 class Solution {
     public long appealSum(String s) {
-        long[] dp = new long[s.length()];
-        dp[0] = 1;
         int[] last = new int[26];
         Arrays.fill(last,-1);
-        long ans = dp[0];
         last[s.charAt(0)-97] = 0;
+        long lastDp = 1l;
+        long ans = 1;
         for(int i=1; i<s.length(); i++)
         {
-            int alphabetIndex = s.charAt(i) - 97;
+            int alphabetIndex = s.charAt(i) - 'a';
+            long currentDp = 0l;
             if(last[alphabetIndex] == -1)
             {
-                dp[i] = dp[i-1] + i+1;
+                currentDp = lastDp + i+1;
             }
             else
             {
-                dp[i] = dp[i-1] + i-last[alphabetIndex];
+                currentDp = lastDp + i-last[alphabetIndex];
             }
             last[alphabetIndex] = i;
-            ans += dp[i];
+            ans += currentDp;
+            lastDp = currentDp;
         }
         return ans;
     }
