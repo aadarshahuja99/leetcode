e50e5eeb6cb8475820f5071e54592ea61e1ac2f3
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-11 16:00:31 +0530

    Time: 0 ms (100%), Space: 41.1 MB (6.91%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,38 +1,23 @@
 class Solution {
     public int maximumSwap(int num) {
-        List<Integer> digits = new ArrayList<>();
-        while(num > 0)
-        {
-            digits.add(num%10);
-            num = num/10;
+        char[] digits = Integer.toString(num).toCharArray();
+        
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
         }
-        Collections.reverse(digits);
-        int[] maxIndex = new int[digits.size()];
-        maxIndex[digits.size()-1] = -1;
-        for(int i=digits.size()-2; i>=0; i--)
-        {
-            int maxIndexSoFar = maxIndex[i+1];
-            if(maxIndexSoFar == -1 || digits.get(i+1) > digits.get(maxIndexSoFar))
-            {
-                maxIndexSoFar = i+1;
-            }
-            maxIndex[i] = maxIndexSoFar;
-        }
-        for(int i=0; i<digits.size()-1; i++)
-        {
-            if(digits.get(i) < digits.get(maxIndex[i]))
-            {
-                int temp = digits.get(i);
-                digits.set(i, digits.get(maxIndex[i]));
-                digits.set(maxIndex[i], temp);
-                break;
+        
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
             }
         }
-        int ans = 0;
-        for(int i=0; i<digits.size(); i++)
-        {
-            ans = ans*10 + digits.get(i);
-        }
-        return ans;
+        
+        return num;
     }
 }
