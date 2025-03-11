1f14edb9e5e070e40a7b18888f72f6edec1ee8d3
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-11 11:42:55 +0530

    Time: 388 ms (14.85%), Space: 77.8 MB (5.05%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int count = 0;
+        int[][] prefixSums = new int[n][26];
+        int[][] suffixSums = new int[n][26];
+        HashSet<Integer>[] sets = new HashSet[26];
+        for(int i=0; i<26; i++)
+        {
+            sets[i] = new HashSet<>();
+        }
+        for(int i=1; i<n; i++)
+        {
+            int c = s.charAt(i-1) - 'a';
+            for(int j=0; j<26; j++)
+            {
+                prefixSums[i][j] = prefixSums[i-1][j];
+            }
+            prefixSums[i][c] += 1;
+        }
+        for(int i=n-2; i>=1; i--)
+        {
+            int c = s.charAt(i+1) - 'a';
+            for(int j=0; j<26; j++)
+            {
+                suffixSums[i][j] = suffixSums[i+1][j];
+                if(j == c)
+                {
+                    suffixSums[i][j] = 1 + suffixSums[i+1][j];
+                }
+                int limit = Math.min(suffixSums[i][j], prefixSums[i][j]);
+                if(!sets[s.charAt(i) - 'a'].contains(j) && limit > 0)
+                {
+                    count++;
+                    sets[s.charAt(i) - 'a'].add(j);
+                }
+            }
+        }
+        return count;
+    }
+}
