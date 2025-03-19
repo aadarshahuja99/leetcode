9eeddb15b6e0e4ecb6ff57307d6da82b8c54d03e
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-03-19 23:44:51 +0530

    Time: 1 ms (35.43%), Space: 44.2 MB (74.51%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -53,7 +53,7 @@ class Solution {
                 {
                     lastGroupEnd.next = unreversedGroupStart;
                 }
-                break;
+                cur = null;
             }
         }
         return head;
