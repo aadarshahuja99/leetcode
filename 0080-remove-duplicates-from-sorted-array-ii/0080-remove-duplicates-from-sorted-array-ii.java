bfd75d34e7ea600d053d5141be7f0d77959e487c
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-04 11:24:42 +0530

    Time: 0 ms (100%), Space: 46.6 MB (62.78%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         // k times generic template
-        int target = 2;
         int n = nums.length;
         if(n <= 2)
         {
             return n;
         }
+        int target = 2;
         int countOfCurrent = 1;
         int pointer = 1;
         for(int i=1; i<n; i++)
