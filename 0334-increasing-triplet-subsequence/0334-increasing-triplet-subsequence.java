ac95e8359fe1d67e5f5345dd862ac09a32c5f106
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-05 18:53:21 +0530

    Time: 2 ms (95.14%), Space: 134.7 MB (7.52%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,51 +1,20 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-        int n = nums.length;
-        int[] nsls = new int[n];
-        int[] ngrs = new int[n];
-        nsls[0] = -1;
-        ngrs[n-1] = -1;
-        stack1.push(0);
-        for(int i=1; i<n; i++)
+        // a less optimized solution would have been to involve LIS and check if an LIS of length three is present in the array. TC would have been nlogn with n extra space
+        // greedy solution
+        int thirdMaxSoFar = Integer.MAX_VALUE;
+        int secondMaxSoFar = Integer.MAX_VALUE;
+        for(int num : nums)
         {
-            while(stack1.size() > 0 && nums[stack1.peek()] >= nums[i])
+            if(thirdMaxSoFar >= num)
             {
-                stack1.pop();
+                thirdMaxSoFar = num;
             }
-            if(stack1.size() == 0)
+            else if(secondMaxSoFar >= num)
             {
-                nsls[i] = -1;
+                secondMaxSoFar = num;
             }
             else
-            {
-                nsls[i] = stack1.peek();
-            }
-            stack1.push(i);
-        }
-
-        stack2.push(n-1);
-        for(int i=n-2; i>=0; i--)
-        {
-            while(stack2.size() > 0 && nums[stack2.peek()] <= nums[i])
-            {
-                stack2.pop();
-            }
-            if(stack2.size() == 0)
-            {
-                ngrs[i] = -1;
-            }
-            else
-            {
-                ngrs[i] = stack2.peek();
-            }
-            stack2.push(i);
-        }
-
-        for(int i=1; i<n-1; i++)
-        {
-            if(nsls[i] != -1 && ngrs[i] != -1)
             {
                 return true;
             }
