6790097d9c52c9f27781ed95373c62a011221564
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-06 06:23:56 +0530

    Time: 8 ms (95.31%), Space: 44.8 MB (82.63%) - LeetHub

--- 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.java
+++ 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.java
@@ -1,5 +1,9 @@
 class Solution {
     public String rearrangeString(String s, int k) {
+        if(k <= 1)
+        {
+            return s;
+        }
         int[][] counts = new int[26][2];
         for(int i=0; i<26; i++)
         {
@@ -9,22 +13,6 @@ class Solution {
         {
             counts[c-97][1]++;
         }
-        // if(k == 0)
-        // {
-        //     String ans = "";
-        //     for(int i=0; i<26; i++)
-        //     {
-        //         if(counts[i][1] > 0)
-        //         {
-        //             char c = (char)(i + 97);
-        //             for(int j=0; j<counts[i][1]; j++)
-        //             {
-        //                 ans += c;
-        //             }
-        //         }
-        //     }
-        //     return ans;
-        // }
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a,b) -> {
             return b[1] - a[1];
         });
@@ -40,12 +28,12 @@ class Solution {
         while(maxHeap.size() > 0)
         {
             int[] top = maxHeap.poll();
-            char c = (char)(97 + top[0]);
+            char c = (char)('a' + top[0]);
             top[1]--;
             // System.out.println(c+" "+top[1]);
             sb.append(c);
             queue.add(top);
-            if(queue.size() == k || k == 0)
+            if(queue.size() == k)
             {
                 var queueTop = queue.poll();
                 if(queueTop[1] > 0)
