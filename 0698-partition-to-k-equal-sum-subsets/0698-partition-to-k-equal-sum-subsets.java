c2e04f0772d4aa43999d9e0bcc396852b5aefbec
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-10 21:27:30 +0530

    Time: 106 ms (58.75%), Space: 55.3 MB (5.26%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -28,31 +28,19 @@ class Solution {
         }
         return cache[state][count] = ans;
     }
-    
-    void reverse(int[] arr) {
-        for (int i = 0, j = arr.length - 1; i < j; i++, j--) { 
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
   
     public boolean canPartitionKSubsets(int[] arr, int k) {
         int totalArraySum = 0;
         int n = arr.length;
-        
         for (int i = 0; i < n; ++i) {
              totalArraySum += arr[i];
         }
-      
         // If total sum not divisible by k, we can't make subsets.
         if (totalArraySum % k != 0) { 
             return false;
         }
-        
         int targetSum = totalArraySum / k;
         Integer mask = 0;
-        
         // Memoize the ans using taken element's string as key.
         Boolean[][] memo = new Boolean[(1<<(n))][k-1];
       
