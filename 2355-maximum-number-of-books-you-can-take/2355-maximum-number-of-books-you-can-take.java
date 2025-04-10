53c5aa3a61ffb64e75763def0231638111a18592
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-10 10:16:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2355-maximum-number-of-books-you-can-take/2355-maximum-number-of-books-you-can-take.java
+++ 2355-maximum-number-of-books-you-can-take/2355-maximum-number-of-books-you-can-take.java
@@ -5,35 +5,27 @@ class Solution {
         cache = new long[books.length];
 
         Stack<Integer> booksShelvesToCheck = new Stack<>();
-        long max = 0;
-        for (int i = 0; i < books.length; i++) {
-            Long result = 0L;    
-
-            if (i == 0) {
-                // first index only
-                result = new Long(books[i]);
-
-            } else if (books[i] == 0) {
+        long max = books[0];
+        cache[0] = books[0];
+        for (int i = 1; i < books.length; i++) {
+            if (books[i] == 0) {
                 // special case when value = 0 so we can instantly return 0
-                result = 0L;                
-
+                cache[i] = 0L;                
             } else if (books[i - 1] < books[i]) {
                 // current element higher than the previous one
-                result = cache[i - 1] + books[i];
-
+                cache[i] = cache[i - 1] + books[i];
             } else {   
-                // current element lower than the previous one
+                // current element lower than the previous one, we can take (books[cur])(books[cur]+1)/2 in the best case but we need to find the limiting element
                 int left = -1; // index of element that should be taken from dp, this will be the limiting element
                 while (!booksShelvesToCheck.isEmpty()) {
                     int shelveToCheck = booksShelvesToCheck.peek();
-                    if (books[shelveToCheck] <= books[i] - (i - shelveToCheck)) {
+                    if (books[shelveToCheck] < books[i] - (i - shelveToCheck)) {
                         left = shelveToCheck;
                         break;
                     } else {
                         booksShelvesToCheck.pop();
                     }
-                }                        
-
+                }
                 if (left >= 0) {
                     // Input:
                     // * left = 2
@@ -44,11 +36,11 @@ class Solution {
                     // books taken:        | 5 6 7  
                     //                    dp(3)
                     
-                    // result = dp(3) + ssum(books[5]) - ssum(books[5] - (i - left))
-                    // result = dp(3) + ssum(7) - ssum(7 - (5 - 2))
-                    // result = dp(3) + ssum(7) - ssum(7 - 3)
-                    // result = dp(3) + ssum(7) - ssum(4)
-                    result = cache[left] + ssum(books[i]) - ssum(books[i] - (i - left));
+                    // cache[i] = dp(3) + ssum(books[5]) - ssum(books[5] - (i - left))
+                    // cache[i] = dp(3) + ssum(7) - ssum(7 - (5 - 2))
+                    // cache[i] = dp(3) + ssum(7) - ssum(7 - 3)
+                    // cache[i] = dp(3) + ssum(7) - ssum(4)
+                    cache[i] = cache[left] + ssum(books[i]) - ssum(books[i] - (i - left));
                 } else {
                     // Input:
                     // * left = -1
@@ -58,16 +50,14 @@ class Solution {
                     // value:          7 7 7 7 7 7
                     // books taken:    2 3 4 5 6 7  
 
-                    // result = ssum(books[5]) - ssum(books[5] - i - 1)
-                    // result = ssum(7) - ssum(7 - 5 - 1)
-                    // result = ssum(7) - ssum(1)
-                    result = ssum(books[i]) - ssum(books[i] - i - 1);
-                }                                                                            
-            }             
-
-            booksShelvesToCheck.add(i);
-            cache[i] = result;
-            max = Math.max(max, result);        
+                    // cache[i] = ssum(books[5]) - ssum(books[5] - i - 1)
+                    // cache[i] = ssum(7) - ssum(7 - 5 - 1)
+                    // cache[i] = ssum(7) - ssum(1)
+                    cache[i] = ssum(books[i]) - ssum(books[i] - i - 1);
+                }
+            }
+            booksShelvesToCheck.push(i);
+            max = Math.max(max, cache[i]);
         }
 
         return max;
