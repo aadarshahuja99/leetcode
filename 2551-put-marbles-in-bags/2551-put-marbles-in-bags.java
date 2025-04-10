9b885a12d792db25e7f6e59b31430c5a1af4b694
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-10 07:05:53 +0530

    Time: 33 ms (81.98%), Space: 62 MB (51.95%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -2,16 +2,16 @@ class Solution {
     public long putMarbles(int[] weights, int k) {
         // VVVVI refer to the editorial for intuition
         int n = weights.length;
-        int[] modified = new int[n-1];
+        int[] splits = new int[n-1];
         for(int i=0; i<n-1; i++)
         {
-            modified[i] = weights[i] + weights[i+1];
+            splits[i] = weights[i] + weights[i+1];
         }
         long ans = 0;
-        Arrays.sort(modified);
+        Arrays.sort(splits);
         for(int i=0; i<k-1; i++)
         {
-            ans += modified[n-2-i] - modified[i];
+            ans += splits[n-2-i] - splits[i];
         }
         return ans;
     }
