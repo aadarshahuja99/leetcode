673d37cb7240f91e8b22ab095210424d3d885867
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-04-10 17:57:12 +0530

    Time: 83 ms (6.77%), Space: 45.7 MB (45.9%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,35 +1,65 @@
 class Solution {
+
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        int numCards = hand.length;
-        if(numCards%groupSize != 0)
+        if(hand.length%groupSize != 0)
         {
             return false;
         }
-        if(groupSize == 1)
-        {
-            return true;
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        for (int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
         }
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        for(int card : hand)
+
+        if(map.size() < groupSize)
         {
-            map.put(card, map.getOrDefault(card,0) + 1);
+            return false;
         }
-        for(int key : map.keySet())
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(var entry : map.entrySet())
         {
-            int currentCount = map.get(key);
-            if(currentCount == 0)
+            minHeap.add(entry.getKey());
+        }
+
+        while(minHeap.size() > 0)
+        {
+            int top = minHeap.poll();
+            HashSet<Integer> set = new HashSet<>();
+            int last = top;
+            for(int i=0; i<groupSize-1; i++)
             {
-                continue;
-            }
-            for(int next = 1; next <= groupSize-1; next++)
-            {
-                if(!map.containsKey(key + next) || map.get(key + next) < map.get(key))
+                if(minHeap.size() == 0)
                 {
                     return false;
                 }
-                map.put(key + next, map.get(key + next) - map.get(key));
+                int next = minHeap.poll();
+                if(map.get(next) == 0 || next != last+1)
+                {
+                    return false;
+                }
+                map.put(next, map.get(next) - 1);
+                if(map.get(next) == 0)
+                {
+                    set.add(next);
+                }
+                last = next;
+            }
+            map.put(top, map.get(top) - 1);
+            if(map.get(top) == 0)
+            {
+                set.add(top);
+            }
+            for(int it = top; it < top + groupSize; it++)
+            {
+                // not adding elements with zero current count to the minHeap
+                if(set.contains(it))
+                {
+                    continue;
+                }
+                minHeap.add(it);
             }
         }
+        // All groups should be completed by the end
         return true;
     }
 }
