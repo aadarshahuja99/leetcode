c2795957d87e9db2810d06fa52f66478f33780fa
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-25 19:29:43 +0530

    Time: 0 ms (100%), Space: 46.2 MB (48.4%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -32,22 +32,27 @@ class Solution {
             // root val is the key
             if(root.left == null && root.right == null)
             {
+                // key node is a leaf node
                 return null;
             }
             if(root.left == null)
             {
+                // if the key node has only one child then replace the key node with the right child
                 return root.right;
             }
             if(root.right == null)
             {
+                // same as previous if
                 return root.left;
             }
+            // if key node is an internal node, then find the inorderPredecessor
             TreeNode inorderPredecessor = root.left;
             while(inorderPredecessor.right != null)
             {
                 inorderPredecessor = inorderPredecessor.right;
             }
             root.val = inorderPredecessor.val;
+            // same as removing the leaf node (inorderPredecessor will always be a leaf node)
             root.left = deleteNode(root.left, inorderPredecessor.val);
         }
         return root;
