9ebc31e55dd8e9ec83b068d6c93ea88c855fd4c9
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-25 12:08:25 +0530

    Time: 7 ms (86.87%), Space: 44.5 MB (97%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -12,11 +12,14 @@ class Solution {
             counts[s.charAt(end) - 'A']++;
             maxFreq = Math.max(counts[s.charAt(end) - 'A'], maxFreq);
             end++;
+            // if the window ending at the end-th guy is invalid, then make it valid
             while(end - start - maxFreq > k)
             {
+                // no need to update the maximum frequency again and again as for example if the current maxFreq is x then the next candidate which will give a possible longer answer has to have the maxFreq to be at least x+1, during shrinking, the maxFreq value will only decrease and that computation is not needed for computing the answer
                 counts[s.charAt(start) - 'A']--;
                 start++;
             }
+            // compute the longest valid window ending at the end-th guy with the answer
             ans = Math.max(ans, end - start);
         }
         return ans;
