eb612eb1c393a240552abb87a2eb4ee56d151359
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-25 13:16:40 +0530

    Time: 1 ms (59.35%), Space: 46.6 MB (41.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,23 +2,23 @@ class Solution {
     public int trap(int[] nums) {
         // intuitive O(N) space solution
         int n = nums.length;
-        int maxSoFar = nums[0];
+        int maxSoFarFromLeft = nums[0];
         int[] rightMax = new int[n];
-        rightMax[n-1] = nums[n-1];
+        rightMax[n-1] = 0;
         Stack<Integer> stack = new Stack<>();
         int ans = 0;
         for(int i=n-2; i>=0; i--)
         {
-            rightMax[i] = Math.max(rightMax[i+1], nums[i]);
+            rightMax[i] = Math.max(rightMax[i+1], nums[i+1]);
         }
         for(int i=1; i<n-1; i++)
         {
-            maxSoFar = Math.max(maxSoFar, nums[i]);
-            int limit = Math.min(rightMax[i], maxSoFar);
+            int limit = Math.min(rightMax[i], maxSoFarFromLeft);
             if(limit > nums[i])
             {
                 ans += limit - nums[i];
             }
+            maxSoFarFromLeft = Math.max(maxSoFarFromLeft, nums[i]);
         }
         return ans;
     }
