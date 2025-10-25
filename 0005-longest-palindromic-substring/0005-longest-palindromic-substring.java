ae0b83655a3b24879b057afa43ab68d9e7b5b7c1
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-25 14:25:11 +0530

    Time: 118 ms (30.47%), Space: 46.3 MB (12.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -14,20 +14,18 @@ class Solution {
                 isPalindrome[i][i+1] = true;
             }
         }
-        int len = 3;
-        for(int k=len; k<=n; k++)
+        for(int length=3; length<=n; length++)
         {
-            for(int start=0; start<=n-k; start++)
+            for(int start=0; start<=n-length; start++)
             {
-                if(s.charAt(start) == s.charAt(start+k-1) && isPalindrome[start+1][start+k-2])
+                if(s.charAt(start) == s.charAt(start+length-1) && isPalindrome[start+1][start+length-2])
                 {
                     ansStart = start;
-                    max = k;
-                    isPalindrome[start][start+k-1] = true;
+                    max = length;
+                    isPalindrome[start][start+length-1] = true;
                 }
             }
         }
-        System.out.println(max+" "+ansStart);
         return s.substring(ansStart, ansStart+max);
     }
 }
