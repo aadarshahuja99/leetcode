d6f2462c869df662867ae35c05849ffaed9da6b8
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-26 09:24:37 +0530

    Time: 108 ms (37.77%), Space: 42.7 MB (65.61%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxCoins(int[] nums) {
+        // a variation of select last MCM, we are selecting the element to be bursted at the end of the current segment at each selection, since at that time there will be no next or prev balloons left on either side of the current balloon, we can safely obtain the coins = val[segmentStart-1]*val[segmentEnd+1]*val[currentIndex] from bursting the current balloon at the end of the segment and recursively call the f(i,k-1) and f(i+1,j) methods since each balloon must be bursted individually. There are no operations between balloons to be done (unlike MCM)
         int[] updated = new int[nums.length+2];
         updated[0] = 1;
         updated[nums.length+1] = 1;
@@ -27,8 +28,8 @@ class Solution {
         int maxCoins = 0;
         for(int k=i; k<=j; k++)
         {
-            // the second argument contains the computation for the last balloon that will be busted
-            maxCoins = Math.max(maxCoins, countMaximumCoins(i,k-1,nums,dp) + countMaximumCoins(k+1,j,nums,dp) + nums[i-1]*nums[k]*nums[j+1]);
+            int costOfCurrentOperationToBePerformedAtEnd = nums[i-1]*nums[k]*nums[j+1];
+            maxCoins = Math.max(maxCoins, countMaximumCoins(i,k-1,nums,dp) + countMaximumCoins(k+1,j,nums,dp) + costOfCurrentOperationToBePerformedAtEnd);
         }
         return dp[i][j] = maxCoins;
     }
