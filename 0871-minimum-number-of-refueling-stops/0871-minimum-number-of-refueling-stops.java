9dbb39e9380ba0f573345e7ae51c945a66629946
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-26 16:05:13 +0530

    Time: 2 ms (96.93%), Space: 45.2 MB (44.17%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.java
@@ -1,33 +1,30 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        Queue<Integer> queue = new PriorityQueue<>((a,b) -> {
+        Queue<Integer> maxHeap = new PriorityQueue<>((a,b) -> {
             return b-a;
         });
-        long dist = startFuel;
-        int res = 0;
+        long levelEnd = startFuel;
+        int reFuels = 0;
         int idx = 0;
         int n = stations.length;
-        while (true) {
-            if(dist >= target)
-            {
-                return res;
-            }
+        while (levelEnd < target) {
             // dist that can be reached so far is less than the target, thus refuelling is required.
             // we greedily choose the station that has already been traversed and can help us get the farthest away from the current level end (i.e dist value)
-            while (idx < n && stations[idx][0] <= dist) {
-                queue.add(stations[idx][1]);
+            while (idx < n && stations[idx][0] <= levelEnd) {
+                maxHeap.add(stations[idx][1]);
                 idx++;
             }
-            if(queue.size() > 0)
+            if(maxHeap.size() > 0)
             {
-                dist += queue.poll(); // for the current level, choose the station that can give the most fuel
+                levelEnd += maxHeap.poll(); // for the current level, choose the station that can give the most fuel
             }
             else
             {
                 // dist is less than target and we do not have refueling stops
                 return -1;
             }
-            res++;
+            reFuels++;
         }
+        return reFuels;
     }
 }
