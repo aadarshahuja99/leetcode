8249396aaecf92d119007738b95cfc2e3be2b620
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-26 15:24:12 +0530

    Time: 2 ms (98.69%), Space: 45.5 MB (81.85%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -5,20 +5,17 @@ class Solution {
         for (char task : tasks) {
             freq[task - 'A']++;
         }
-
         // Sort the frequency array in non-decreasing order
         Arrays.sort(freq);
         // Calculate the maximum frequency of any task
         int groups = freq[25] - 1;
         // Calculate the number of idle slots that will be required
         int idleSlots = groups * n;
-
         // Iterate over the frequency array from the second highest frequency to the lowest frequency
         for (int i = 24; i >= 0 && freq[i] > 0; i--) {
             // Subtract the minimum of the maximum frequency and the current frequency from the idle slots
             idleSlots -= Math.min(groups, freq[i]);
         }
-
         // If there are any idle slots left, add them to the total number of tasks
         return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
     }
