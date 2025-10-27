42eccf7b6e18a28f0720434dcd1b4fab119a2024
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-27 08:20:13 +0530

    Time: 1 ms (68.15%), Space: 45.6 MB (84.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -9,19 +9,19 @@ class Solution {
             {
                 if(matrix[i][j] == 0)
                 {
-                    matrix[i][0] = 0;
+                    matrix[i][0] = 0; // indicates that ith row must be set to 0
                     if(j == 0)
                     {
-                        col0 = 0;
+                        col0 = 0; // indicates that 0th column must be set to 0
                     }
                     else
                     {
-                        matrix[0][j] = 0;
+                        matrix[0][j] = 0; // indicates that j-th column must be set to 0
                     }
                 }
             }
         }
-
+        // update the inner elements of the matrix on the basis of the 2 arrays that we have used in place for storing status
         for(int i=1; i<m; i++)
         {
             for(int j=1; j<n; j++)
@@ -32,17 +32,17 @@ class Solution {
                 }
             }
         }
-
         if(matrix[0][0] == 0)
         {
+            // set all values in the first row as 0
             for(int j=0; j<n; j++)
             {
                 matrix[0][j] = 0;
             }
         }
-
         if(col0 == 0)
         {
+            // set all values in the first column as 0
             for(int i=0; i<m; i++)
             {
                 matrix[i][0] = 0;
