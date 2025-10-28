74226bf24610cc019dbc1b1f13fb59e8975f308e
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-28 14:17:26 +0530

    Time: 6 ms (23.06%), Space: 52.2 MB (21.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,35 +2,45 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] visited = new int[m][n];
         int count = 0;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] vis = new boolean[m][n];
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(visited[i][j] == 0 && grid[i][j] == '1')
+                if(grid[i][j] == '1' && !vis[i][j])
                 {
-                    dfs(i,j,visited,grid);
+                    bfs(i,j,q,vis,grid);
                     count++;
                 }
             }
         }
         return count;
     }
-    private void dfs(int row, int col, int[][] visited, char[][] grid)
+    private void bfs(int sr, int sc, Queue<int[]> q, boolean[][] vis, char[][] grid)
     {
-        visited[row][col] = 1;
-        int[] deltaRow = new int[] { 0,1,0,-1 };
-        int[] deltaColumn = new int[] { 1,0,-1,0 };
-        int m = visited.length;
-        int n = visited[0].length;
-        for(int i=0; i<4; i++)
+        int[][] delta = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        q.add(new int[] { sr, sc });
+        while(q.size() > 0)
         {
-            int newRow = row + deltaRow[i];
-            int newColumn = col + deltaColumn[i];
-            if(newRow >= 0 && newRow < m && newColumn >= 0 && newColumn < n && visited[newRow][newColumn] == 0 && grid[newRow][newColumn] == '1')
+            int[] top = q.poll();
+            int r = top[0];
+            int c = top[1];
+            for(int[] d : delta)
             {
-                dfs(newRow, newColumn, visited, grid);
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if(nr < 0 || nr == vis.length || nc < 0 || nc == vis[0].length)
+                {
+                    continue;
+                }
+                if(vis[nr][nc] || grid[nr][nc] == '0')
+                {
+                    continue;
+                }
+                vis[nr][nc] = true;
+                q.add(new int[] { nr, nc });
             }
         }
     }
