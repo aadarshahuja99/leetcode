ccd9752e6a8c7d1bd3cc8329790ba1a5fdedb727
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-28 21:18:16 +0530

    Time: 3 ms (15.12%), Space: 45.7 MB (7.78%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -15,31 +15,31 @@
  */
 class Solution {
     public int rob(TreeNode root) {
-        HashMap<TreeNode,HashMap<Integer,Integer>> cache = new HashMap<>();
+        HashMap<TreeNode,int[]> cache = new HashMap<>();
         return getMaxMoney(root, 1, cache);
     }
-    private int getMaxMoney(TreeNode root, int canRob, HashMap<TreeNode,HashMap<Integer,Integer>> cache)
+    private int getMaxMoney(TreeNode root, int canRob, HashMap<TreeNode, int[]> cache)
     {
         if(root == null)
         {
             return 0;
         }
-        if(cache.containsKey(root) && cache.get(root).containsKey(canRob))
+        if(cache.containsKey(root) && cache.get(root)[canRob] != -1)
         {
-            return cache.get(root).get(canRob);
+            return cache.get(root)[canRob];
         }
         if(!cache.containsKey(root))
         {
-            cache.put(root, new HashMap<>());
+            cache.put(root, new int[] { -1, -1 });
         }
         int dontRob = getMaxMoney(root.left, 1, cache) + getMaxMoney(root.right, 1, cache);
         if(canRob == 1)
         {
             int maxAns = Math.max(root.val + getMaxMoney(root.left, 0, cache) + getMaxMoney(root.right, 0, cache), dontRob);
-            cache.get(root).put(canRob, maxAns);
+            cache.get(root)[canRob] = maxAns;
             return maxAns;
         }
-        cache.get(root).put(canRob, dontRob);
+        cache.get(root)[canRob] = dontRob;
         return dontRob;
     }
 }
