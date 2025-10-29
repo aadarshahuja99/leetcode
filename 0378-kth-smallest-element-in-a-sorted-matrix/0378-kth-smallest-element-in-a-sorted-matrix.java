9dfa2c27da68f6a746692b260c4b3b9c1489d386
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-29 13:21:46 +0530

    Time: 17 ms (46.85%), Space: 47.9 MB (91.63%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,47 +1,23 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        // brute-force. Worst case: (n2)log(n2)
-            // if(matrix.length == 1 || k == 1)
-            // {
-            //     return matrix[0][0];
-            // }
-            // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-            // for(int[] row : matrix)
-            // {
-            //     for(int element : row)
-            //     {
-            //         pq.add(element);
-            //         if(pq.size() > k)
-            //         {
-            //             pq.poll();
-            //         }
-            //     }
-            // }
-            // return pq.peek();
-        // more efficient solution (inspired from k sorted arrays problem):
-        // if matrix[i][j] is taken as the next smallest element, add matrix[i][j+1] to the pq. The complexity for this approach is: O(klogN)
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            public int compare(int[] element1, int[] element2)
-            {
-                return element1[0] - element2[0];
-            }
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> {
+            return a[0]-b[0];
         });
         int n = matrix.length;
         for(int i=0; i<n; i++)
         {
-            pq.add(new int[] { matrix[i][0], i, 0 });
+            minHeap.add(new int[] { matrix[i][0], i, 0 });
         }
-        while(k > 1)
+        while(k-- > 1)
         {
-            int[] top = pq.poll();
-            int row = top[1];
-            int column = top[2];
-            if(column < n-1)
+            int[] top = minHeap.poll();
+            int rowIndex = top[1];
+            int colIndex = top[2];
+            if(colIndex+1 < n)
             {
-                pq.add(new int[] { matrix[row][column+1], row, column+1 });
+                minHeap.add(new int[] { matrix[rowIndex][colIndex+1], rowIndex, colIndex+1 });
             }
-            k--;
         }
-        return pq.peek()[0];
+        return minHeap.peek()[0];
     }
 }
