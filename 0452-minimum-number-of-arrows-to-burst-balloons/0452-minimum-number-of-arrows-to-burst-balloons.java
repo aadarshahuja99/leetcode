69b9d9bc2b9f87db8fa998a346c7a23bf7db96ab
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-10-29 15:58:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,19 +1,11 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
         // not a range sum problem
-        Arrays.sort(points, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b)
-            {
-                if(a[1] == b[1])
-                {
-                    return Long.compare(a[0],b[0]);
-                }
-                return Long.compare(a[1],b[1]);
-            }
+        Arrays.sort(points, (a,b) -> {
+            return a[0] - b[0];
         });
         int currentEnd = points[0][1];
         int ans = 1;
-        // System.out.println(points[0][0] + " " + points[0][1]);
         for(int i=1; i<points.length; i++)
         {
             int[] current = points[i];
