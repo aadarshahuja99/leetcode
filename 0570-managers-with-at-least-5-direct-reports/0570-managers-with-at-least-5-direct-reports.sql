a31caede86b5e111bf07b7bd6aa19e48ef29f648
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-11-01 16:39:12 +0530

    Time: 347 ms (40.2%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,19 @@
-# Write your MySQL query statement below
-with query as (
-    select e.name as name from (
-        select distinct employee.managerId as id, COUNT(employee.id) OVER (PARTITION BY employee.managerId) as employee_count
-        from employee
-    ) t, Employee e where t.employee_count >= 5 AND t.id = e.id
+WITH ManagerReports AS (
+    SELECT
+        managerId,
+        COUNT(*) AS manager_report_count
+    FROM
+        Employee
+    WHERE
+        managerId IS NOT NULL
+    GROUP BY
+        managerId
+    HAVING
+        manager_report_count >= 5
 )
-select query.name from query;
+SELECT
+    e.name
+FROM
+    Employee e
+INNER JOIN
+    ManagerReports mr ON e.id = mr.managerId;
