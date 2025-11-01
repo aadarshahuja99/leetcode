363d0a9b625e74bc5f2232ab97d45014ab86d3f2
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-11-01 16:44:17 +0530

    Time: 530 ms (96.88%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -6,4 +6,4 @@ cte_count as (
     select SUM(CASE WHEN min_date = customer_pref_delivery_date THEN 1 ELSE 0 END) as valid_count from
     Delivery inner join cte on cte.customer_id = Delivery.customer_id
 )
-select ROUND((cte_count.valid_count/(select count(distinct customer_id) from Delivery))*100, 3) as immediate_percentage from cte_count;
+select ROUND((cte_count.valid_count/(select count(distinct customer_id) from Delivery))*100, 2) as immediate_percentage from cte_count;
