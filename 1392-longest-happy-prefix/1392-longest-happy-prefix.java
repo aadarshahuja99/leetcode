10abada0aad03bf4acd2fdcf175b35936bbc914b
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2025-11-03 09:49:54 +0530

    Time: 11 ms (47.89%), Space: 45.6 MB (32.27%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String longestPrefix(String s) {
+        if(s.length() == 1)
+        {
+            return "";
+        }
+        int i=1;
+        int prev=0;
+        int[] lps = new int[s.length()];
+        while(i<s.length())
+        {
+            if(s.charAt(i) == s.charAt(prev))
+            {
+                lps[i] = prev + 1; // if there is a match, set the current longest prefix match length to prev plus 1.
+                i++;
+                prev++;
+            }
+            else if(prev == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+            else
+            {
+                prev = lps[prev-1];
+            }
+        }
+        if(lps[s.length()-1] == 0)
+        {
+            return "";
+        }
+        return s.substring(0,lps[s.length()-1]);
+    }
+}
