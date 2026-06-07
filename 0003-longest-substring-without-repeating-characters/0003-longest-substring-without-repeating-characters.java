86f96611767b96c8ae4eb684056bcc118e0488e8
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-06-07 17:34:20 +0530

    Time: 7 ms (36.27%), Space: 45.1 MB (88.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,15 +7,18 @@ class Solution {
         int n = s.length();
         while(j < n)
         {
+            // consume the jth guy (expand the window)
             char cj = s.charAt(j);
             counts.put(cj, counts.getOrDefault(cj, 0) + 1);
             j++;
+            // post consumption check if the window is invalid, if yes then shrink the window from the start to find the longest window that ends at the jth guy
             while(counts.get(cj) > 1)
             {
                 char c = s.charAt(i);
                 counts.put(c, counts.get(c) - 1);
                 i++;
             }
+            // compare the length of the longest window ending at the jth guy with the overall longest window
             ans = Math.max(ans, j - i);
         }
         return ans;
