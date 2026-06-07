bc9643e96bea736f9aa6c5c61b2dd278d57e6716
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-06-07 17:10:45 +0530

    Time: 8 ms (77.04%), Space: 46.4 MB (94.52%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -6,7 +6,6 @@ class Solution {
         while(i < n)
         {
             int mask = 0;
-            HashSet<Character> set = new HashSet<>();
             int j=i;
             while(j < n && (mask&(1<<(s.charAt(j) - 'a'))) == 0)
             {
