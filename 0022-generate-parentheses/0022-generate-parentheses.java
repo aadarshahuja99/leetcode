375c82303b8eed362f59ad18122fb97b12f229a9
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-06-21 20:59:48 +0530

    Time: 1 ms (86.76%), Space: 44.1 MB (92.98%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,27 +1,33 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> allCombinations = new ArrayList<String>();
-        getParenthesis(0, 0, "", allCombinations, n);
+        getParenthesis(0, 0, new StringBuilder(), allCombinations, n);
         return allCombinations;
     }
-    private void getParenthesis(int currentIndex, int opened, String currentString, List<String> allCombinations, int n)
+    private void getParenthesis(int currentIndex, int opened, StringBuilder currentString, List<String> allCombinations, int n)
     {
         if(currentIndex == 2*n)
         {
             if(opened == 0)
             {
-                allCombinations.add(currentString);
+                allCombinations.add(new String(currentString));
             }
             return;
         }
         if(opened > 0)
         {
-            getParenthesis(currentIndex+1, opened+1, currentString+"(", allCombinations, n);
-            getParenthesis(currentIndex+1, opened-1, currentString+")", allCombinations, n);
+            currentString.append("(");
+            getParenthesis(currentIndex+1, opened+1, currentString, allCombinations, n);
+            currentString.deleteCharAt(currentString.length() - 1);
+            currentString.append(")");
+            getParenthesis(currentIndex+1, opened-1, currentString, allCombinations, n);
+            currentString.deleteCharAt(currentString.length() - 1);
         }
         else
         {
-            getParenthesis(currentIndex+1, opened+1, currentString+"(", allCombinations, n);
+            currentString.append("(");
+            getParenthesis(currentIndex+1, opened+1, currentString, allCombinations, n);
+            currentString.deleteCharAt(currentString.length() - 1);
         }
     }
 }
