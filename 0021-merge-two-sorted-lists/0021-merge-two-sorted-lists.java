f19214e1e8903b960a08950e25092f1562d36140
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-02 07:19:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (57.01%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode current = null;
+        ListNode head = null;
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val > list2.val)
+            {
+                if(current == null)
+                {
+                    current = list2;
+                }
+                else
+                {
+                    current.next = list2;
+                    current = current.next;
+                }
+                list2 = list2.next;
+            }
+            else
+            {
+                if(current == null)
+                {
+                    current = list1;
+                }
+                else
+                {
+                    current.next = list1;
+                    current = current.next;
+                }
+                list1 = list1.next;
+            }
+            if(head == null)
+            {
+                head = current;
+            }
+            current.next = null;
+        }
+        if(list1 != null)
+        {
+            if(current == null)
+            {
+                current = list1;
+                head = current;
+            }
+            else
+            {
+                current.next = list1;
+            }
+        }
+        if(list2 != null)
+        {
+            if(current == null)
+            {
+                current = list2;
+                head = current;
+            }
+            else
+            {
+                current.next = list2;
+            }
+        }
+        return head;
+    }
+}
