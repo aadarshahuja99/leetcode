46951536c1247646e0a598da12c0b350b52b65a8
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-07 07:08:39 +0530

    Time: 28 ms (8.82%), Space: 51.6 MB (46.17%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans = 0;
+        int sum = 0;
+        for(int num : nums)
+        { 
+            sum += num;
+            // Reminders can not be negative in Maths but Java does not follow this, (-3)%5 in java = -3
+            // but the correct answer in maths is 2 for it
+            // (sum%k + k)%k will ensure correct reminders are found and not java based reminders in case of negative numbers 
+            if(map.containsKey((sum%k + k)%k))
+            {
+                ans += map.get((sum%k + k)%k);
+            }
+            if((sum%k + k)%k == 0)
+            {
+                ans++;
+            }
+            map.put((sum%k + k)%k, map.getOrDefault((sum%k + k)%k, 0) + 1);
+        }
+        return ans;
+    }
+}
