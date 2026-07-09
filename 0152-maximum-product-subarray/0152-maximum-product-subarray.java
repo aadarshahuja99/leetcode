3255b053476bb3df9d8fe7a02b4e940551c7d580
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-09 07:02:47 +0530

    Time: 1 ms (93.09%), Space: 47 MB (87.64%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,6 +4,13 @@ class Solution {
         int suffixProd = 1;
         int n = nums.length;
         int ans = Integer.MIN_VALUE;
+
+        // intuition:
+        // 1. Ans will always be a subarray from start or subarray from end because of these 3 cases:
+        // a. If all positive, ans = prod of the complete array
+        // b. If even negative, ans = prod of the complete array
+        // c. If odd negative, ans = prod of the subarray from either start or either end excluding one of the negative numbers
+        // If zeroes are present, then we solve individually for each of the non-zero including segments, this is achieved by setting prefixProd, suffixProd to 1 (if they ever become zero) 
         for(int i=0; i<n; i++)
         {
             prefixProd = nums[i]*prefixProd;
