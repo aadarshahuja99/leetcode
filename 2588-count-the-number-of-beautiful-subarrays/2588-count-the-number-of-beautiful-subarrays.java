a7bbdf2869b4d94c886dc7758f9fca8c2f01830d
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-09 06:43:01 +0530

    Time: 62 ms (26.53%), Space: 114.2 MB (50.61%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.java
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.java
@@ -3,22 +3,15 @@ class Solution {
         HashMap<Integer,Long> map = new HashMap<>();
         long ans = 0;
         int xor = 0;
+        map.put(0, 1L);
         for(int num : nums)
         {
             xor = xor^num;
             if(map.containsKey(xor))
             {
                 ans += map.get(xor);
-                map.put(xor, map.get(xor)+1L);
             }
-            else
-            {
-                map.put(xor,1L);
-            }
-            if(xor == 0)
-            {
-                ans += 1L;
-            }
+            map.put(xor, map.getOrDefault(xor,0L) + 1L);
         }
         return ans;
     }
