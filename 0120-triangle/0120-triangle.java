beafb35994b024725bab66b452c0cf2050fe8bf0
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-10 21:50:15 +0530

    Time: 5 ms (35.88%), Space: 45.3 MB (74.84%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -19,18 +19,18 @@ class Solution {
         }
         return next[0];
     }
-    private int getAns(int row, int col, List<List<Integer>> triangle, int[][] cache)
-    {
-        if(row == triangle.size())
-        {
-            return 0;
-        }
-        if(cache[row][col] != -1)
-        {
-            return cache[row][col];
-        }
-        int moveToSameCol = triangle.get(row).get(col) + getAns(row+1, col, triangle, cache);
-        int moveToDiffCol = triangle.get(row).get(col) + getAns(row+1, col+1, triangle, cache);
-        return cache[row][col] = Math.min(moveToSameCol, moveToDiffCol);
-    }
+    // private int getAns(int row, int col, List<List<Integer>> triangle, int[][] cache)
+    // {
+    //     if(row == triangle.size())
+    //     {
+    //         return 0;
+    //     }
+    //     if(cache[row][col] != -1)
+    //     {
+    //         return cache[row][col];
+    //     }
+    //     int moveToSameCol = triangle.get(row).get(col) + getAns(row+1, col, triangle, cache);
+    //     int moveToDiffCol = triangle.get(row).get(col) + getAns(row+1, col+1, triangle, cache);
+    //     return cache[row][col] = Math.min(moveToSameCol, moveToDiffCol);
+    // }
 }
