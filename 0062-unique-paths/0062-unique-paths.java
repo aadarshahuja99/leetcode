6da26a7fb20d829cb1a1e38907e7fa3ad6bc8b08
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-10 07:09:17 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,14 @@
-import java.math.BigInteger;
 class Solution {
     public int uniquePaths(int m, int n) {
-        return getNCR(m+n-2, m-1);
-    }
-    // combinatorics
-    private int getNCR(int n, int r)
-    {
-        BigInteger numerator = BigInteger.valueOf(1);
-        BigInteger denomenator = BigInteger.valueOf(1);
-        for(int i=0; i<r; i++)
-        {
-            numerator = numerator.multiply(BigInteger.valueOf(n-i));
-            denomenator = denomenator.multiply(BigInteger.valueOf(i+1));
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
         }
-        return (numerator.divide(denomenator)).intValue();
+        return dp[m - 1][n - 1];
     }
 }
