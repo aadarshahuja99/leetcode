4c91da055291d207190a9c3bc1012fe54ebefc5a
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-11 11:20:31 +0530

    Time: 1 ms (80.74%), Space: 43.2 MB (49.21%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,13 +1,33 @@
 class Solution {
     public int numDecodings(String s) {
-        int[] cache = new int[s.length()];
-        Arrays.fill(cache, -1);
-        int ans = getAns(0, s, cache);
-        if(ans == Integer.MIN_VALUE)
+        if(s.charAt(0) == '0')
         {
             return 0;
         }
-        return ans;
+        int n = s.length();
+        int[] cache = new int[n+1];
+        cache[n] = 1;
+        for(int i=n-1; i>=0; i--)
+        {
+            int num = 0;
+            int current = 0;
+            for(int p = i; p<n; p++)
+            {
+                num = num*10 + (s.charAt(p) - '0');
+                if(num > 26)
+                {
+                    break;
+                }
+                if(p+1 < n && s.charAt(p+1) == '0')
+                {
+                    continue;
+                }
+                current += cache[p+1];
+            }
+            cache[i] = current;
+        }
+        // int ans = getAns(0, s, cache);
+        return cache[0];
     }
     private int getAns(int current, String s, int[] cache)
     {
@@ -19,10 +39,6 @@ class Solution {
         {
             return cache[current];
         }
-        if(s.charAt(current) == '0')
-        {
-            return cache[current] = Integer.MIN_VALUE;
-        }
         int num = 0;
         int ans = 0;
         for(int i=current; i<Math.min(current+2, s.length()); i++)
@@ -32,12 +48,11 @@ class Solution {
             {
                 break;
             }
-            int val = getAns(i+1, s, cache);
-            // System.out.println("for start = "+current+" and num = "+num+" val = "+val);
-            if(val == Integer.MIN_VALUE)
+            if(i+1 < s.length() && s.charAt(i+1) == 0)
             {
                 continue;
             }
+            int val = getAns(i+1, s, cache);
             ans += val;
         }
         return cache[current] = ans;
