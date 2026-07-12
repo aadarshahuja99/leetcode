f1c872bd4837c9661d57dfc5beef9797f43f663b
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-12 05:53:11 +0530

    Time: 113 ms (34.11%), Space: 44.4 MB (48.73%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        // a variation of select last MCM, we are selecting the element to be bursted at the end of the current segment at each selection, since at that time there will be no next or prev balloons left on either side of the current balloon, we can safely obtain the coins = val[segmentStart-1]*val[segmentEnd+1]*val[currentIndex] from bursting the current balloon at the end of the segment and recursively call the f(i,k-1) and f(i+1,j) methods since each balloon must be bursted individually. There are no operations between balloons to be done (unlike MCM)
+        // a variation of select last MCM, we are selecting the element to be bursted at the end of the current segment at each selection, since at that time there will be no next or prev balloons left on either side of the current balloon, we can safely obtain the coins = val[segmentStart-1]*val[segmentEnd+1]*val[currentIndex] from bursting the current balloon as the last balloon of the segment and recursively call the f(i,k-1) and f(k+1,j) methods since each balloon must be bursted individually. There are no operations between balloons to be done (unlike MCM)
         int[] updated = new int[nums.length+2];
         updated[0] = 1;
         updated[nums.length+1] = 1;
@@ -28,8 +28,8 @@ class Solution {
         int maxCoins = 0;
         for(int k=i; k<=j; k++)
         {
-            int costOfCurrentOperationToBePerformedAtEnd = nums[i-1]*nums[k]*nums[j+1];
-            maxCoins = Math.max(maxCoins, countMaximumCoins(i,k-1,nums,dp) + countMaximumCoins(k+1,j,nums,dp) + costOfCurrentOperationToBePerformedAtEnd);
+            int costOfCurrentOperationToBePerformedAtLast = nums[i-1]*nums[k]*nums[j+1];
+            maxCoins = Math.max(maxCoins, countMaximumCoins(i,k-1,nums,dp) + countMaximumCoins(k+1,j,nums,dp) + costOfCurrentOperationToBePerformedAtLast);
         }
         return dp[i][j] = maxCoins;
     }
