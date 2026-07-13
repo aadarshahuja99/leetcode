d2bc66b86b9421f6f023205da062b29edc597339
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-13 06:58:34 +0530

    Time: 0 ms (100%), Space: 43 MB (15.14%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return dfs(p, q);
+        // return dfs(p, q);
+        return iterative(p, q);
     }
     private boolean dfs(TreeNode c1, TreeNode c2)
     {
@@ -24,5 +25,43 @@ class Solution {
             return c1 == c2;
         }
         return c1.val == c2.val && dfs(c1.left, c2.left) && dfs(c1.right, c2.right);
+    }
+    private boolean iterative(TreeNode c1, TreeNode c2)
+    {
+        TreeNode current1 = c1;
+        TreeNode current2 = c2;
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        while(true)
+        {
+            if(current1 == null || current2 == null)
+            {
+                if(current1 != current2)
+                {
+                    return false;
+                }
+                if(s1.isEmpty() || s2.isEmpty())
+                {
+                    return s1.isEmpty() && s2.isEmpty();
+                }
+                if(s1.peek().val != s2.peek().val)
+                {
+                    return false;
+                }
+                current1 = s1.pop().right;
+                current2 = s2.pop().right;
+            }
+            else
+            {
+                if(current1.val != current2.val)
+                {
+                    return false;
+                }
+                s1.push(current1);
+                s2.push(current2);
+                current1 = current1.left;
+                current2 = current2.left;
+            }
+        }
     }
 }
