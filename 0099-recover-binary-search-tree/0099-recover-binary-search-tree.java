7b8032632eb59957bcbdff24006d299cf4575865
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-16 00:17:13 +0530

    Time: 1 ms (100%), Space: 46.5 MB (70.59%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,85 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public void recoverTree(TreeNode root) {
-        // morris inorder traversal + last popped element
-        TreeNode lastNode = null;
-        TreeNode firstIncorrect = null;
-        TreeNode lastIncorrect = null;
-        TreeNode current = root;
-        while(current != null)
-        {
-            if(current.left == null)
-            {
-                var changed = helper(current,lastNode,firstIncorrect,lastIncorrect);
-                firstIncorrect = changed.getKey();
-                lastIncorrect = changed.getValue();
-                lastNode = current;
-                current = current.right;
-            }
-            else
-            {
-                TreeNode leftChild = current.left;
-                while(leftChild.right != null && leftChild.right != current)
-                {
-                    leftChild = leftChild.right;
+        TreeNode a = null, b = null, prev = null;
+        // Morris Traversal
+        TreeNode node = root;
+        while(node != null) {
+            if(node.left == null) {
+                // Specific to this problem (Not Required in Morris Traversal), this is being done to check if the inorder predecessor is more in value than the current node, if so, then it is an anomaly
+                if(prev != null && prev.val > node.val) {
+                    if (a == null) {
+                        a = prev;
+                    }
+                    b = node;
                 }
-                if(leftChild.right == current)
-                {
-                    var changed = helper(current,lastNode,firstIncorrect,lastIncorrect);
-                    firstIncorrect = changed.getKey();
-                    lastIncorrect = changed.getValue();
-                    leftChild.right = null;
-                    lastNode = current;
-                    current = current.right;
-                }
-                else
-                {
-                    leftChild.right = current;
-                    current = current.left;
+                prev = node;
+                node = node.right;
+            } else {
+                TreeNode predecessor = findPredecessor(node.left, node);
+                if(predecessor.right == null) {
+                    predecessor.right = node;
+                    node = node.left;
+                } else {
+                    predecessor.right = null;
+					// Specific to this problem (Not Required in Morris Traversal), this is being done to check if the inorder predecessor is more in value than the current node, if so, then it is an anomaly
+                    if(prev != null && prev.val > node.val) {
+                        if(a == null) {
+                            a = prev;
+                        }
+                        b = node;
+                    }
+                    prev = node;
+                    node = node.right;
                 }
             }
         }
-        int temp = firstIncorrect.val;
-        firstIncorrect.val = lastIncorrect.val;
-        lastIncorrect.val = temp;
-        return;
+        // simply swap the two incorrect values
+        int data = a.val;
+        a.val = b.val;
+        b.val = data;
     }
-    private Pair<TreeNode,TreeNode> helper(TreeNode current, TreeNode lastNode, TreeNode firstIncorrect, TreeNode lastIncorrect)
-    {
-        if(lastNode == null)
-        {
-            lastNode = current;
+    
+    TreeNode findPredecessor(TreeNode node, TreeNode cur) {
+        while(node.right != null && node.right != cur) {
+            node = node.right;
         }
-        else
-        {
-            int currentValue = current.val;
-            if(currentValue < lastNode.val)
-            {
-                if(firstIncorrect == null)
-                {
-                    firstIncorrect = lastNode;
-                    lastIncorrect = current;
-                }
-                else
-                {
-                    lastIncorrect = current;
-                }
-            }
-        }
-        return new Pair<TreeNode,TreeNode>(firstIncorrect, lastIncorrect);
+        return node;
     }
 }
