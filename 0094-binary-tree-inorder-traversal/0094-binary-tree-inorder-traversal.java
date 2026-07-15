4e46380a2c6b9beabb8245cdb31676beace7c31b
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-15 05:30:43 +0530

    Time: 0 ms (100%), Space: 43.3 MB (15.04%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -15,6 +15,42 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        TreeNode current = root;
+        List<Integer> ans = new ArrayList<>();
+        while(current != null)
+        {
+            if(current.left == null)
+            {
+                ans.add(current.val);
+                current = current.right;
+            }
+            else
+            {
+                // this is the right-most guy in the left subtree
+                TreeNode inorderPredecessor = current.left;
+                while(inorderPredecessor.right != null && inorderPredecessor.right != current)
+                {
+                    inorderPredecessor = inorderPredecessor.right;
+                }
+                // left sub-tree has not been visited to far, so we vist it
+                if(inorderPredecessor.right == null)
+                {
+                    inorderPredecessor.right = current;
+                    current = current.left;
+                }
+                else
+                {
+                    // left subtree has already been visited because we have a link from inorderPredecessor to the current node
+                    inorderPredecessor.right = null;
+                    ans.add(current.val);
+                    current = current.right;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public List<Integer> inorderUsingStack(TreeNode root) {
         Stack<TreeNode> st = new Stack<>();
         TreeNode current = root;
         List<Integer> ans = new ArrayList<>();
