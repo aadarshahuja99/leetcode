9dcf43f1398d17b85c6643e7d229d1cd6d0ae855
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-15 05:34:21 +0530

    Time: 0 ms (100%), Space: 42.5 MB (99.07%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,28 +14,76 @@
  * }
  */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        // iterative
-        Stack<TreeNode> st = new Stack<>();
+    public List<Integer> preorderTraversal(TreeNode root)
+    {
         TreeNode current = root;
         List<Integer> ans = new ArrayList<>();
-        while(true)
+        while(current != null)
         {
-            if(current != null)
+            if(current.left == null)
             {
                 ans.add(current.val);
-                st.push(current);
-                current = current.left;
+                current = current.right;
             }
             else
             {
-                if(st.size() == 0)
+                // this is the right-most guy in the left subtree
+                TreeNode inorderPredecessor = current.left;
+                while(inorderPredecessor.right != null && inorderPredecessor.right != current)
                 {
-                    return ans;
+                    inorderPredecessor = inorderPredecessor.right;
                 }
-                var top = st.pop();
-                current = top.right;
+                // left sub-tree has not been visited to far, so we vist it
+                if(inorderPredecessor.right == null)
+                {
+                    // since we are in pre-order, we put current to ans before visiting left sub-tree
+                    ans.add(current.val);
+                    inorderPredecessor.right = current;
+                    current = current.left;
+                }
+                else
+                {
+                    // left subtree has already been visited because we have a link from inorderPredecessor to the current node
+                    inorderPredecessor.right = null;
+                    current = current.right;
+                }
             }
         }
+        return ans;
+    }
+
+    public List<Integer> preorderTraversalUsingStack(TreeNode root) {
+        // Initialize a stack to track nodes
+        Stack<TreeNode> st = new Stack<>();
+        // Start from the root node
+        TreeNode node = root;
+        // Initialize a list to store
+        // inorder traversal result
+        List<Integer> preorder = new ArrayList<>();
+        // Start an infinite
+        // loop for traversal
+        while (true) {
+            // If the current node is not NULL
+            if (node != null) {
+                // Push the current
+                // node to the stack
+                st.push(node);
+                preorder.add(node.val);
+                // Move to the left child
+                // of the current node
+                node = node.left;
+            } else {
+                // If the stack is empty,
+                // break the loop
+                if (st.empty()) {
+                    break;
+                }
+                // Retrieve a node from the stack
+                node = st.pop().right;
+            }
+        }
+        // Return the inorder
+        // traversal result
+        return preorder;
     }
 }
