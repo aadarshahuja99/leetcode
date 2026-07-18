79383077cca98b6582a9562a4ea38221059a283d
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-18 20:21:55 +0530

    Time: 13 ms (62.72%), Space: 51.3 MB (87.18%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -4,69 +4,50 @@ class Solution {
         {
             return 1;
         }
-        ArrayList<ArrayList<QueueElement>> adjList = new ArrayList<ArrayList<QueueElement>>();
+        ArrayList<ArrayList<long[]>> adjList = new ArrayList<ArrayList<long[]>>();
         for(int i=0; i<n; i++)
         {
-            adjList.add(new ArrayList<QueueElement>());
+            adjList.add(new ArrayList<long[]>());
         }
         for(int[] road : roads)
         {
-            adjList.get(road[0]).add(new QueueElement(road[1],(long)road[2]));
-            adjList.get(road[1]).add(new QueueElement(road[0],(long)road[2]));
+            adjList.get(road[0]).add(new long[] { 1l*road[1],(long)road[2] });
+            adjList.get(road[1]).add(new long[] { 1l*road[0],(long)road[2] });
         }
         int[] paths = new int[n];
         long[] distances = new long[n];
         Arrays.fill(distances,Long.MAX_VALUE);
         distances[0] = 0;
         paths[0] = 1;
-        PriorityQueue<QueueElement> pq = new PriorityQueue<QueueElement>(new Comparator<QueueElement>() {
-            public int compare(QueueElement q1, QueueElement q2)
-            {
-                return Long.compare(q1.dist,q2.dist);
-            }
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b) -> {
+            return Long.compare(a[1], b[1]);
         });
-        pq.add(new QueueElement(0,0));
+        pq.add(new long[] { 0l, 0l });
+        boolean[] vis = new boolean[n];
         while(pq.size() > 0)
         {
-            QueueElement top = pq.poll();
-            int index = top.getIndex();
-            long dist = top.getDistance();
-            for(QueueElement node : adjList.get(index))
+            var top = pq.poll();
+            int index = (int)top[0];
+            long dist = top[1];
+            if(vis[index])
             {
-                if(distances[node.getIndex()] > dist + node.getDistance())
+                continue;
+            }
+            vis[index] = true;
+            for(long[] node : adjList.get(index))
+            {
+                if(distances[(int)node[0]] > dist + node[1])
                 {
-                    distances[node.getIndex()] = dist + node.getDistance();
-                    paths[node.getIndex()] = paths[index];
-                    pq.add(new QueueElement(node.getIndex(),distances[node.getIndex()]));
+                    distances[(int)node[0]] = dist + node[1];
+                    paths[(int)node[0]] = paths[index];
+                    pq.add(new long[] { node[0], distances[(int)node[0]] });
                 }
-                else if(distances[node.getIndex()] == dist + node.getDistance())
+                else if(distances[(int)node[0]] == dist + node[1])
                 {
-                    paths[node.getIndex()] = ((paths[node.getIndex()])%1000000007 + (paths[index])%1000000007)%1000000007;
+                    paths[(int)node[0]] = ((paths[(int)node[0]]) + (paths[index]))%1000000007;
                 }
             }
         }
         return paths[n-1];
-    }
-    class QueueElement
-    {
-        private int index;
-        private long dist;
-        public QueueElement(int i, long d)
-        {
-            index = i;
-            dist = d;
-        }
-        public void setDistance(long d)
-        {
-            dist = d;
-        }
-        public long getDistance()
-        {
-            return dist;
-        }
-        public int getIndex()
-        {
-            return index;
-        }
     }
 }
