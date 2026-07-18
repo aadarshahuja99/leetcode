c4a3420c0f521d88c48965b1d990e4675cfbe3c7
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-18 20:17:03 +0530

    Time: 23 ms (31.58%), Space: 47.4 MB (12.82%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,32 +1,38 @@
 class Solution {
+    int[] deltaR = { 1,0,-1,0 };
+    int[] deltaC = { 0,1,0,-1 };
     public int minCost(int[][] grid) {
-        if(grid.length == 1 && grid[0].length == 1)
-        {
-            return 0;
-        }
+        int m = grid.length;
+        int n = grid[0].length;
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
             public int compare(int[] q1, int[] q2)
             {
                 return q1[0] - q2[0];
             }
         });
-        int[][] distances = new int[grid.length][grid[0].length];
+        int[][] distances = new int[m][n];
         for(int[] d : distances)
         {
             Arrays.fill(d,Integer.MAX_VALUE);
         }
         distances[0][0] = 0;
         pq.add(new int[] { 0,0,0 });
+        boolean[][] visited = new boolean[m][n];
         while(pq.size() > 0)
         {
             var top = pq.poll();
             int row = top[1];
             int col = top[2];
             int dist = top[0];
-            if(row == grid.length - 1 && col == grid[0].length - 1)
+            if(row == m - 1 && col == n - 1)
             {
                 return dist;
             }
+            if(visited[row][col])
+            {
+                continue;
+            }
+            visited[row][col] = true;
             int iR = row;
             int iC = col;
             if(grid[row][col] == 1)
@@ -45,22 +51,20 @@ class Solution {
             {
                 iR -= 1;
             }
-            int[] deltaR = { 1,0,-1,0 };
-            int[] deltaC = { 0,1,0,-1 };
             for(int i=0; i<4; i++)
             {
                 int nR = row + deltaR[i];
                 int nC = col + deltaC[i];
-                if(nR >=0 && nR<grid.length && nC>=0 && nC <grid[0].length)
+                if(nR >= 0 && nR < m && nC >= 0 && nC < n && !visited[nR][nC])
                 {
-                    int factor = 1;
+                    int cost = 1;
                     if(iR == nR && iC == nC)
                     {
-                        factor = 0;
+                        cost = 0;
                     }
-                    if(distances[nR][nC] > dist + factor)
+                    if(distances[nR][nC] > dist + cost)
                     {
-                        distances[nR][nC] = dist + factor;
+                        distances[nR][nC] = dist + cost;
                         pq.add(new int[] { distances[nR][nC],nR,nC });
                     }
                 }
