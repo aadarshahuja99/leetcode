1e05e9528684ae7773605437866b566ba8c10c20
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-19 11:27:54 +0530

    Time: 11 ms (72.18%), Space: 142 MB (70.49%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,65 +1,62 @@
 class Solution {
     public int minScore(int n, int[][] roads) {
-        int[] visited = new int[n];
-        ArrayList<ArrayList<Element>> adjList = new ArrayList<ArrayList<Element>>();
-        int[] min = new int[n];
-        Arrays.fill(min,Integer.MAX_VALUE);
-        for(int i=0; i<n; i++)
+        // can be done using bfs/dfs more intuitively. Can also be done using Union Find
+        DS ds = new DS(n+1);
+        int ans = 0;
+        for(int[] road : roads)
         {
-            adjList.add(new ArrayList<Element>());
+            ds.union(road[0], road[1]);
+            ans = Math.max(road[2], ans);
         }
         for(int[] road : roads)
         {
-            adjList.get(road[0]-1).add(new Element(road[1]-1,road[2]));
-            adjList.get(road[1]-1).add(new Element(road[0]-1,road[2]));
-            if(road[2] < min[road[0]-1])
+            if(ds.findParent(road[0]) == ds.findParent(n))
             {
-                min[road[0]-1]=road[2];
-            }
-            if(road[2] < min[road[1]-1])
-            {
-                min[road[1]-1]=road[2];
-            }
-        }
-        dfs(0,visited,adjList);
-        int ans = Integer.MAX_VALUE;
-        for(int i=0; i<n; i++)
-        {
-            // System.out.println("current min for i =  "+ i + " is " + min[i]);
-            if(visited[i] != 0)
-            {
-                ans = Math.min(ans,min[i]);
+                ans = Math.min(ans, road[2]);
             }
         }
         return ans;
     }
-    private void dfs(int current, int[] visited, ArrayList<ArrayList<Element>> adjList)
+    class DS
     {
-        visited[current] = 1;
-        for(Element element : adjList.get(current))
+        int[] p;
+        int[] s;
+        DS(int n)
         {
-            if(visited[element.getNode()] != 1)
+            p = new int[n];
+            s = new int[n];
+            for(int i=0; i<n; i++)
             {
-                dfs(element.getNode(),visited,adjList);
+                p[i] = i;
+                s[i] = 1;
             }
         }
-    }
-    class Element
-    {
-        int node;
-        int distance;
-        public Element(int n, int d)
+        public int findParent(int u)
         {
-            node = n;
-            distance = d;
+            if(p[u] == u)
+            {
+                return u;
+            }
+            return p[u] = findParent(p[u]);
         }
-        public int getNode()
+        public void union(int u, int v)
         {
-            return node;
-        }
-        public int getDistance()
-        {
-            return distance;
+            int pU = findParent(u);
+            int pV = findParent(v);
+            if(pU == pV)
+            {
+                return;
+            }
+            if(s[pU] >= s[pV])
+            {
+                p[pV] = pU;
+                s[pU] += s[pV];
+            }
+            else
+            {
+                p[pU] = pV;
+                s[pV] += s[pU];
+            }
         }
     }
 }
