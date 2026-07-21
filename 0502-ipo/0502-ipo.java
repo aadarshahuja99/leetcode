9c1f8f71f7f40a88720523718eb88a446faac0f2
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-21 21:43:19 +0530

    Time: 95 ms (83.25%), Space: 133.1 MB (61.61%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,58 +1,31 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<int[]> doableProjects = new PriorityQueue<>(new Comparator<int[]>() {
-            public int compare(int[] a, int[] b)
-            {
-                return b[1] - a[1];
-            }
-        });
-        PriorityQueue<int[]> candidates = new PriorityQueue<>(new Comparator<int[]>() {
-            public int compare(int[] a, int[] b)
-            {
-                return a[0] - b[0];
-            }
-        });
-        // candidates contains the projects that haven't been processed/marked as selectable. Once a project is selectable, we pop it from the candidates pq and add it to the doableProjects pq, then we greedily select the project with the highest profit from the doable projects
-        int[][] elems = new int[profits.length][2];
-        boolean canAnyProjectBeStarted = false;
-        for(int i=0; i<profits.length; i++)
+        int n = profits.length;
+        int[][] combined = new int[n][2];
+        for(int i=0; i<n; i++)
         {
-            if(capital[i] <= w)
+            combined[i][0] = capital[i];
+            combined[i][1] = profits[i];
+        }
+        Arrays.sort(combined, (a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        int count = 0;
+        int money = w;
+        int idx = 0;
+        while(count < k)
+        {
+            while(idx < n && combined[idx][0] <= money)
             {
-                canAnyProjectBeStarted = true;
+                pq.add(combined[idx][1]);
+                idx++;
             }
-            int[] newElement = new int[2];
-            newElement[0] = capital[i];
-            newElement[1] = profits[i];
-            elems[i] = newElement;
-        }
-        if(!canAnyProjectBeStarted)
-        {
-            return 0;
-        }
-        for(int[] elem : elems)
-        {
-            candidates.add(elem);
-        }
-        int current = w;
-        while(candidates.size() > 0 && k > 0)
-        {
-            while(candidates.size() > 0 && candidates.peek()[0] <= current)
+            if(pq.size() == 0)
             {
-                doableProjects.add(candidates.poll());
+                return money;
             }
-            if(doableProjects.size() == 0)
-            {
-                return current;
-            }
-            current += doableProjects.poll()[1];
-            k--;
+            money += pq.poll();
+            count++;
         }
-        while(k > 0 && doableProjects.size() > 0)
-        {
-            current += doableProjects.poll()[1];
-            k--;
-        }
-        return current;
+        return money;
     }
 }
