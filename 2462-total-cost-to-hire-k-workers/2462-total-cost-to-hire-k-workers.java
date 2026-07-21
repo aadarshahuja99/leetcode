d11a96d83496cb4b60c96f4318972a0b66cdea03
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-21 07:21:02 +0530

    Time: 52 ms (87.27%), Space: 78.7 MB (35.51%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -2,68 +2,37 @@ class Solution {
     public long totalCost(int[] costs, int k, int candidates) {
         int n = costs.length;
         boolean[] added = new boolean[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            if(a[0] == b[0])
+        PriorityQueue<Integer> pqStart = new PriorityQueue<>();
+        PriorityQueue<Integer> pqEnd = new PriorityQueue<>();
+        int hired = 0;
+        int start = 0;
+        int end = n-1;
+        long ans = 0;
+        while(hired < k)
+        {
+            while(pqStart.size() < candidates && start <= end)
             {
-                return a[1] - b[1];
+                pqStart.add(costs[start]);
+                start++;
             }
-            return a[0] - b[0];
-        });
-        int start = -1;
-        for(int i=0; i<Math.min(candidates,n); i++)
-        {
-            start++;
-            pq.add(new int[] { costs[i], i, 0 });
-            added[i] = true;
-        }
-        int end = n;
-        for(int i=n-1; i>=Math.max(0, n-candidates); i--)
-        {
-            if(added[i])
+            while(pqEnd.size() < candidates && end >= start)
             {
-                break;
-            }
-            end--;
-            pq.add(new int[] { costs[i], i, 1 });
-            added[i] = true;
-        }
-        int selected = 0;
-        long ans = 0l;
-        while(selected < k && start < end)
-        {
-            int[] top = pq.poll();
-            // System.out.println("adding "+top[0]+" "+top[1]+" "+top[2]);
-            ans += top[0];
-            selected++;
-            if(top[2] == 1)
-            {
+                pqEnd.add(costs[end]);
                 end--;
-                if(end == start)
-                {
-                    // System.out.println("breaking");
-                    break;
-                }
-                // System.out.println("queuing "+costs[end]+" "+end+" "+1);
-                pq.add(new int[] { costs[end], end, 1 });
+            }
+            int firstMin = pqStart.size() > 0 ? pqStart.peek() : Integer.MAX_VALUE;
+            int secondMin = pqEnd.size() > 0 ? pqEnd.peek() : Integer.MAX_VALUE;
+            if(firstMin <= secondMin)
+            {
+                ans += firstMin;
+                pqStart.poll();
             }
             else
             {
-                start++;
-                if(end == start)
-                {
-                    // System.out.println("breaking");
-                    break;
-                }
-                // System.out.println("queuing "+costs[start]+" "+start+" "+0);
-                pq.add(new int[] { costs[start], start, 0 });
+                ans += secondMin;
+                pqEnd.poll();
             }
-        }
-        while(selected < k)
-        {
-            var top = pq.poll();
-            // System.out.println("adding "+top[0]+" "+top[1]+" "+top[2]);
-            ans += top[0];
-            selected++;
+            hired++;
         }
         return ans;
     }
