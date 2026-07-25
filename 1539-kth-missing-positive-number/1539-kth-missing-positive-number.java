bd7d4e9205e4fb2bbc188167368f9ed2291405cc
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-25 15:19:04 +0530

    Time: 0 ms (100%), Space: 44.4 MB (88.17%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -6,6 +6,7 @@ class Solution {
             if (arr[mid] - (mid + 1) >= k) r = mid-1;  //missed more or equal than k numbers, left side;
             else l = mid + 1;   // missed less than k numbers, must be in the right side;
         }
-        return l + k;
+        // answer will be from formula: arr[r] + (k - (arr[r] - (r+1))); the formula simplifies to r+1 + k
+        return r+1+k;
     }
 }
