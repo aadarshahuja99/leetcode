232fa6ec598e801688a35aabfd81028e583f823a
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-25 19:42:29 +0530

    Time: 1 ms (86.08%), Space: 44.4 MB (73.32%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,32 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> allCombinations = new ArrayList<String>();
-        getParenthesis(0, 0, new StringBuilder(), allCombinations, n);
+        getParenthesis(0, 0, 0, new StringBuilder(), allCombinations, n);
         return allCombinations;
     }
-    private void getParenthesis(int currentIndex, int opened, StringBuilder currentString, List<String> allCombinations, int n)
+    private void getParenthesis(int currentIndex, int opened, int closed, StringBuilder currentString, List<String> allCombinations, int n)
     {
         if(currentIndex == 2*n)
         {
-            if(opened == 0)
-            {
-                allCombinations.add(new String(currentString));
-            }
+            allCombinations.add(new String(currentString));
             return;
         }
-        if(opened > 0)
+        if(opened < n)
         {
             currentString.append("(");
-            getParenthesis(currentIndex+1, opened+1, currentString, allCombinations, n);
-            currentString.deleteCharAt(currentString.length() - 1);
-            currentString.append(")");
-            getParenthesis(currentIndex+1, opened-1, currentString, allCombinations, n);
+            getParenthesis(currentIndex+1, opened+1, closed, currentString, allCombinations, n);
             currentString.deleteCharAt(currentString.length() - 1);
         }
-        else
+        if (opened > closed)
         {
-            currentString.append("(");
-            getParenthesis(currentIndex+1, opened+1, currentString, allCombinations, n);
+            currentString.append(")");
+            getParenthesis(currentIndex+1, opened, closed+1, currentString, allCombinations, n);
             currentString.deleteCharAt(currentString.length() - 1);
         }
     }
