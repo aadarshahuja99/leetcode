9c76561f24aa6d16f6ccba8a6f6758e6a2560b0a
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-27 06:48:29 +0530

    Time: 6 ms (99.21%), Space: 74.3 MB (99.01%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.java
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.java
@@ -1,34 +1,28 @@
 class Solution {
+
     public int minMoves(int[] nums, int limit) {
-        // 3 cases for each pair:
-        // targetSum = currentSum -> no changes
-        // targetSum >= 2 and targetSum < min(A,B) + 1 -> change the max and min both
-        // targetSum >= min(A,B) + 1 and targetSum <= max(A,B) + limit -> update only the min one of A and B
-        // targetSum > max(A,B) + limit and targetSum <= 2*limit -> update both
-        int[] counts = new int[2*limit + 2];
-        int[] changes = new int[2*limit + 2];
         int n = nums.length;
-        int pairs = n/2;
-        for(int i=0; i<n/2; i++)
-        {
-            int other = n-i-1;
-            int min = Math.min(nums[other], nums[i]);
-            changes[min+1]++;
-            changes[Math.max(nums[other], nums[i]) + limit + 1]--;
-            counts[nums[other] + nums[i]]++;
+        int[] diff = new int[2 * limit + 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = Math.min(nums[i], nums[n - 1 - i]);
+            int b = Math.max(nums[i], nums[n - 1 - i]);
+
+            diff[2] += 2;
+            diff[a + 1] -= 1;
+            diff[a + b] -= 1;
+            diff[a + b + 1] += 1;
+            diff[b + limit + 1] += 1;
         }
-        // for(int i=0; i<counts.length; i++)
-        // {
-        //     System.out.println(i+" "+counts[i]);
-        // }
-        int totalChanges = 0;
-        int ans = n;
-        for(int current=2; current <= 2*limit; current++)
-        {
-            totalChanges += changes[current];
-            // System.out.println(counts[current]+", "+totalChanges+" for "+current);
-            ans = Math.min(totalChanges + 2*(n/2 - totalChanges) - counts[current], ans);
+
+        int minOps = n;
+        int currentOps = 0;
+
+        for (int c = 2; c <= 2 * limit; c++) {
+            currentOps += diff[c];
+            minOps = Math.min(minOps, currentOps);
         }
-        return ans;
+
+        return minOps;
     }
 }
