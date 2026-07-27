45052ecf81b7bd02acfc8d45df582af5c3072431
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-27 21:53:38 +0530

    Time: 59 ms (9.73%), Space: 115.7 MB (78%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -5,14 +5,15 @@ class Solution {
                 return a[1] == b[1] ? a[0] - b[0] : a[1] - b[1];
             });
             int end = intervals[0][1];
-            int count = 1;
-
+            int keepCount = 1;
+            // keep the earliest ending interval
+            // example: [1,100] [2,3] [4,5] [6,7] if we sort by increasing starts then we will only be able to keep the first interval. As a result, sorting by increasing ends makes us keep the earliest ending interval as it will have the least overlap and occupy the least amount of timeline.
             for (int i = 1; i < intervals.length; i++) {
                 if (intervals[i][0] >= end) {
                     end = intervals[i][1];
-                    count++;
+                    keepCount++;
                 }
             }
-            return intervals.length - count;
+            return intervals.length - keepCount;
         }
 }
