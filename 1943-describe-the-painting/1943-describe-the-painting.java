8998f013ebbf5d07388d0443bfed27a59166fc99
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-28 08:13:19 +0530

    Time: 132 ms (28%), Space: 125.8 MB (51.2%) - LeetHub

--- 1943-describe-the-painting/1943-describe-the-painting.java
+++ 1943-describe-the-painting/1943-describe-the-painting.java
@@ -3,23 +3,18 @@ import java.util.Map.Entry;
 class Solution {
     public List<List<Long>> splitPainting(int[][] segments) {
         TreeMap<Integer, Long> map = new TreeMap<>();
-
         for(int segment[]: segments) {
             map.put(segment[0], map.getOrDefault(segment[0], 0L) + segment[2]);
             map.put(segment[1], map.getOrDefault(segment[1], 0L) - segment[2]);
         }
-
         List<List<Long>> result = new ArrayList<>();
-
         int prev = 0;
         long sum = 0;
-
+        // every single segment point (either start or end will be making its own contribution to the colour running "sum", so we only focus on the starts and ends of segments to compute answer based on running sum till the current start or end point)
         for(int key: map.keySet()) {
-            // System.out.println(prev+" "+key+" "+sum);
             if(sum != 0) { // Ignore the unpainted interval
                 result.add(Arrays.asList(1l*prev, 1l*key, sum)); // Add the interval
             }
-
             sum += map.get(key);
             prev = key;
         }
