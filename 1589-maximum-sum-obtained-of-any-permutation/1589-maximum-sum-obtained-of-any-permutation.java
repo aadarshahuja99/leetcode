f6224ea2023555d26475bd09e0a0845aa4ca675a
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-28 06:33:33 +0530

    Time: 28 ms (91.14%), Space: 111.6 MB (31%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.java
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.java
@@ -1,57 +1,54 @@
 class Solution {
-    public int maxSumRangeQuery(int[] nums, int[][] requests) {
-        Arrays.sort(requests, (a, b) -> {
-            return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
-        });
-        int n = nums.length;
-        int it = 0;
-        int currentEnd = -1;
-        int length = 0;
-        while(it < requests.length)
-        {
-            int j = it+1;
-            int start = requests[it][0];
-            currentEnd = requests[it][1];
-            while(j < requests.length && requests[j][0] <= currentEnd)
+    // public int maxSumRangeQuery(int[] nums, int[][] requests) {
+    //     Arrays.sort(requests, (a, b) -> {
+    //         return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+    //     });
+    //     int n = nums.length;
+    //     Arrays.sort(nums);
+    //     int[] counts = new int[n+1];
+    //     for(int[] req : requests)
+    //     {
+    //         counts[req[0]]++;
+    //         counts[req[1] + 1]--;
+    //     }
+    //     int total = 0;
+    //     for(int i=1; i<n; i++)
+    //     {
+    //         counts[i] += counts[i-1];
+    //     }
+    //     Arrays.sort(counts);
+    //     long ans = 0l;
+    //     int mod = 1_000_000_007;
+    //     int it = 0;
+    //     while(it < n)
+    //     {
+    //         // System.out.println(pq.peek()+" "+counts[it]);
+    //         ans = (ans%mod + (((1l*counts[it]))*((nums[it]*1l)))%mod)%mod;
+    //         it++;
+    //     }
+    //     return (int)ans;
+    // }
+
+        public int maxSumRangeQuery(int[] A, int[][] req) {
+        long res = 0, mod = (long)1e9 + 7;
+        int n = A.length, count[] = new int[n+1];
+        for (int[] r: req) {
+            count[r[0]] += 1;
+            if(r[1] + 1 < n)
             {
-                currentEnd = Math.max(currentEnd, requests[j][1]);
-                j++;
-            }
-            it = j;
-            length += currentEnd - start + 1;
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
-            return a - b;
-        });
-        for(int num : nums)
-        {
-            pq.add(num);
-            if(pq.size() > length)
-            {
-                pq.poll();
+                count[r[1] + 1] -= 1;
             }
         }
-        int[] counts = new int[n+1];
-        for(int[] req : requests)
+        for (int i = 1; i <= n; i++)
         {
-            counts[req[0]]++;
-            counts[req[1] + 1]--;
+            count[i] += count[i - 1];
         }
-        int total = 0;
-        for(int i=1; i<n; i++)
+        Arrays.sort(A);
+        Arrays.sort(count, 0, n);
+        for (int i = 0; i < n; i++)
         {
-            counts[i] += counts[i-1];
+            res += (long)A[i] * count[i];
         }
-        Arrays.sort(counts);
-        long ans = 0l;
-        int mod = 1_000_000_007;
-        it = n - length + 1;
-        while(pq.size() > 0)
-        {
-            // System.out.println(pq.peek()+" "+counts[it]);
-            ans = (ans%mod + (((1l*counts[it]))*((pq.poll()*1l)))%mod)%mod;
-            it++;
-        }
-        return (int)ans;
+        return (int)(res % mod);
     }
 }
