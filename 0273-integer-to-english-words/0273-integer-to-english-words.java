cfe77b3be56d04cb3a38e09161ddc40dd329b9c8
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-07-31 06:16:44 +0530

    Time: 11 ms (5.22%), Space: 44.3 MB (65.39%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -5,28 +5,23 @@ class Solution {
 
     public String numberToWords(int num) {
         if (num == 0) return "Zero";
-
         int i = 0;
         StringBuilder words = new StringBuilder();
-        
         while (num > 0) {
             if (num % 1000 != 0)
                 words.insert(0, String.format("%s%s ",helper(num % 1000),THOUSANDS[i]));
             num /= 1000;
             i++;
         }
-        
         return words.toString().trim();
     }
-
     private String helper(int num) {
         if (num == 0)
             return "";
         else if (num < 20)
             return LESS_THAN_20[num] + " ";
         else if (num < 100)
-            return TENS[num / 10] + " " + helper(num % 10);
-        else
-            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+            return TENS[num / 10] + " " + helper(num%10);
+        return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
     }
 }
