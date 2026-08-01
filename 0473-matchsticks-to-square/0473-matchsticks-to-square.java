f0deac9dc994a82a36f2eb7e7c8053c7d660247e
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-08-01 10:54:03 +0530

    Time: 70 ms (48.8%), Space: 50 MB (5.45%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        if(matchsticks.length < 4)
+        {
+            return false;
+        }
+        return canPartitionKSubsets(matchsticks,4);
+    }
+        private boolean getAns(int state, int count, int sum, int target, int k, int[] arr, Boolean[][] cache) {
+        int n = arr.length;
+        // We made k - 1 subsets with target sum and last subset will also have target sum.
+        if (count == k - 1) { 
+            return true;
+        }
+        if(sum == target)
+        {
+            return getAns(state, count+1, 0, target, k, arr, cache);
+        }
+        // sum is not a part of cache key as it is directly derivable from 'state' key.
+        if(cache[state][count] != null)
+        {
+            return cache[state][count];
+        }
+        boolean ans = false;
+        for(int i=0; i<n; i++)
+        {
+            if((state&(1<<i)) == 0 && (sum + arr[i]) <= target)
+            {
+                ans = ans | getAns((state^(1<<i)), count, sum+arr[i], target, k, arr, cache);
+                if(ans)
+                {
+                    return cache[state][count] = true;
+                }
+            }
+        }
+        return cache[state][count] = ans;
+    }
+  
+    public boolean canPartitionKSubsets(int[] arr, int k) {
+        int totalArraySum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+             totalArraySum += arr[i];
+        }
+        // If total sum not divisible by k, we can't make subsets.
+        if (totalArraySum % k != 0) { 
+            return false;
+        }
+        int targetSum = totalArraySum / k;
+        Integer mask = 0;
+        // Memoize the ans using taken element's string as key.
+        Boolean[][] memo = new Boolean[(1<<(n))][k-1];
+      
+        return getAns(0, 0, 0, targetSum, k, arr, memo);
+    }
+}
