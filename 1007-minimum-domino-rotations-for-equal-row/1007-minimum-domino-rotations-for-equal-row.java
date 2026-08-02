c8aca635fd3c9793d0f77beddbb17348cd725eb0
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-08-02 18:16:40 +0530

    Time: 4 ms (55.61%), Space: 52.9 MB (42.99%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,45 +1,37 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int min = tops.length+1;
-        int[][][] cache = new int[tops.length][7][2];
-        for(int[][] outer : cache)
+        int n = tops.length;
+        int ans = n+1;
+        for(int number=1; number<=6; number++)
         {
-            for(int[] row : outer)
+            boolean status = true;
+            int countTops = 0;
+            int countBottoms = 0;
+            for(int i=0; i<n; i++)
             {
-                Arrays.fill(row, -1);
+                if(tops[i] != number && bottoms[i] != number)
+                {
+                    status = false;
+                    break;
+                }
+                else if(tops[i] == bottoms[i])
+                {
+                    continue;
+                }
+                else if(tops[i] == number)
+                {
+                    countTops++;
+                }
+                else
+                {
+                    countBottoms++;
+                }
+            }
+            if(status)
+            {
+                ans = Math.min(ans, Math.min(countTops, countBottoms));
             }
         }
-        for(int i=1; i<=6; i++)
-        {
-            for(int face=0; face<=1; face++)
-            {
-                min = Math.min(min, getAns(0, i, face, tops, bottoms, cache));
-            }
-        }
-        return min > tops.length ? -1 : min;
-    }
-    private int getAns(int current, int desiredVal, int desiredRow, int[] tops, int[] bottoms, int[][][] cache)
-    {
-        if(current == tops.length)
-        {
-            return 0;
-        }
-        if(cache[current][desiredVal][desiredRow] != -1)
-        {
-            return cache[current][desiredVal][desiredRow];
-        }
-        if(tops[current] != desiredVal && bottoms[current] != desiredVal)
-        {
-            return cache[current][desiredVal][desiredRow] = tops.length+1;
-        }
-        if(tops[current] == desiredVal && bottoms[current] == desiredVal)
-        {
-            return cache[current][desiredVal][desiredRow] = getAns(current+1, desiredVal, desiredRow, tops, bottoms, cache);
-        }
-        if((tops[current] == desiredVal && desiredRow == 1) || (bottoms[current] == desiredVal && desiredRow == 0))
-        {
-            return cache[current][desiredVal][desiredRow] = 1 + getAns(current+1, desiredVal, desiredRow, tops, bottoms, cache);
-        }
-        return cache[current][desiredVal][desiredRow] = getAns(current+1, desiredVal, desiredRow, tops, bottoms, cache);
+        return ans == n+1 ? -1 : ans;
     }
 }
