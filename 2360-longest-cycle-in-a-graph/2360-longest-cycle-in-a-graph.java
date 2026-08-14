59341998dddf27ab1745438363771f3f0e9927ea
aadarshahuja99 <2017.aadarsh.ahuja@ves.ac.in>
2026-08-14 07:27:22 +0530

    Time: 25 ms (67.14%), Space: 116.6 MB (36.17%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -13,30 +13,30 @@ class Solution {
         }
         return ans;
     }
-    private Pair<Integer,Integer> dfs(int current, int[] edges, int[] visited, int[] pathVisited)
+    private int[] dfs(int current, int[] edges, int[] visited, int[] pathVisited)
     {
         visited[current] = 1;
         pathVisited[current] = 1;
         if(edges[current] != -1 && visited[edges[current]] == 0)
         {
             var status = dfs(edges[current], edges, visited, pathVisited);
-            if(status.getKey() != -1)
+            if(status[0] != -1)
             {
-                int dist = status.getValue();
-                if(status.getKey() == current)
+                int dist = status[1];
+                if(status[0] == current)
                 {
                     // update the ans and return -1 as source to the parent
                     ans = Math.max(ans, dist+1);
-                    return new Pair<>(-1,0);
+                    return new int[] { -1,0 };
                 }
-                return new Pair<>(status.getKey(), dist+1);
+                return new int[] { status[0], dist+1 };
             }
         }
         else if(edges[current] != -1 && pathVisited[edges[current]] == 1)
         {
-            return new Pair<>(edges[current], 1);
+            return new int[] { edges[current], 1 };
         }
         pathVisited[current] = 0;
-        return new Pair<>(-1,0);
+        return new int[] { -1,0 };
     }
 }
